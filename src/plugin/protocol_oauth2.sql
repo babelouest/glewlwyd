3669341c4d014016c059668d98e82bbec52d659f
babelouest <github@babelouest.org>
2019-02-22 12:59:37 -0500

    Complete all response types, fix memory leaks, clean code, fight the bad guys, save the world

--- src/plugin/protocol_oauth2.sql
+++ src/plugin/protocol_oauth2.sql
@@ -32,18 +32,20 @@ CREATE TABLE `gpg_refresh_token` (
   `gpgr_client_id` VARCHAR(256),
   `gpgr_issued_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gpgr_expires_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gpgr_last_seen` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `gpgr_duration` INT(11),
   `gpgr_rolling_expiration` TINYINT(1) DEFAULT 0,
   `gpgr_issued_for` VARCHAR(256), -- IP address or hostname
   `gpgr_token_hash` VARCHAR(512) NOT NULL,
+  `gpgr_enabled` TINYINT(1) DEFAULT 1,
   FOREIGN KEY(`gpgc_id`) REFERENCES `gpg_code`(`gpgc_id`) ON DELETE CASCADE
 );
 CREATE INDEX `i_gpgr_token_hash` ON `gpg_refresh_token`(`gpgr_token_hash`);
 
 CREATE TABLE `gpg_refresh_token_scope` (
-  `gpgrts_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+  `gpgrs_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
   `gpgr_id` INT(11),
-  `gpgrts_scope` VARCHAR(128) NOT NULL,
+  `gpgrs_scope` VARCHAR(128) NOT NULL,
   FOREIGN KEY(`gpgr_id`) REFERENCES `gpg_refresh_token`(`gpgr_id`) ON DELETE CASCADE
 );
 
@@ -52,7 +54,7 @@ CREATE TABLE `gpg_access_token` (
   `gpga_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
   `gpga_authorization_type` INT(2) NOT NULL, -- 0: Authorization Code Grant, 1: Implicit Grant, 2: Resource Owner Password Credentials Grant, 3: Client Credentials Grant
   `gpgr_id` INT(11) DEFAULT NULL,
-  `gpga_username` VARCHAR(256) NOT NULL,
+  `gpga_username` VARCHAR(256),
   `gpga_client_id` VARCHAR(256),
   `gpga_issued_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `gpga_issued_for` VARCHAR(256), -- IP address or hostname
@@ -60,8 +62,8 @@ CREATE TABLE `gpg_access_token` (
 );
 
 CREATE TABLE `gpg_access_token_scope` (
-  `gpgats_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+  `gpgas_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
   `gpga_id` INT(11),
-  `gpgats_scope` VARCHAR(128) NOT NULL,
+  `gpgas_scope` VARCHAR(128) NOT NULL,
   FOREIGN KEY(`gpga_id`) REFERENCES `gpg_access_token`(`gpga_id`) ON DELETE CASCADE
 );
