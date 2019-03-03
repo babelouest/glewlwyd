3d28fe47e99e0097fd6ce4c5cac6a70d01128725
babelouest <github@babelouest.org>
2019-03-03 13:22:35 -0500

    Update header files because by default, Glewlwyd is no longer providing JWTs, only its OAuth2 plugin does

--- src/glewlwyd-common.h
+++ src/glewlwyd-common.h
@@ -5,7 +5,6 @@
  * Authentiation server
  * Users are authenticated via various backend available: database, ldap
  * Using various authentication methods available: password, OTP, send code, etc.
- * Provides Json Web Tokens (jwt)
  * 
  * Declarations for common constants and prototypes used in Glewlwyd main program and modules
  *
