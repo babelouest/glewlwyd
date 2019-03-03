e61e493f9cfec9e2d22cd768fdbf86bf78078c87
babelouest <github@babelouest.org>
2019-03-03 10:06:39 -0500

    Test when add user that another one with the same username does not exist

--- src/webservice.c
+++ src/webservice.c
@@ -1050,16 +1050,27 @@ int callback_glewlwyd_get_user (const struct _u_request * request, struct _u_res
 
 int callback_glewlwyd_add_user (const struct _u_request * request, struct _u_response * response, void * user_data) {
   struct config_elements * config = (struct config_elements *)user_data;
-  json_t * j_user, * j_user_valid;
+  json_t * j_user, * j_user_valid, * j_search_user, * j_body;
   
   j_user = ulfius_get_json_body_request(request, NULL);
   if (j_user != NULL) {
     j_user_valid = is_user_valid(config, NULL, j_user, 1, u_map_get(request->map_url, "source"));
     if (check_result_value(j_user_valid, G_OK)) {
-      if (add_user(config, j_user, u_map_get(request->map_url, "source")) != G_OK) {
-        y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_add_user - Error add_user");
+      j_search_user = get_user(config, json_string_value(json_object_get(j_user, "username")), u_map_get(request->map_url, "source"));
+      if (check_result_value(j_search_user, G_ERROR_NOT_FOUND)) {
+        if (add_user(config, j_user, u_map_get(request->map_url, "source")) != G_OK) {
+          y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_add_user - Error add_user");
+          response->status = 500;
+        }
+      } else if (check_result_value(j_search_user, G_OK)) {
+        j_body = json_pack("{sis[s]}", "result", G_ERROR_PARAM, "error", "username already exists");
+        ulfius_set_json_body_response(response, 400, j_body);
+        json_decref(j_body);
+      } else {
+        y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_add_user - Error get_user");
         response->status = 500;
       }
+      json_decref(j_search_user);
     } else if (check_result_value(j_user_valid, G_ERROR_PARAM)) {
       ulfius_set_json_body_response(response, 400, json_object_get(j_user_valid, "user"));
     } else {
