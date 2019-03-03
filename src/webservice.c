532fb5a0353bc55adb5ef69171a44665f4aded31
babelouest <github@babelouest.org>
2019-03-02 19:52:47 -0500

    Implement user CRUD endpoints

--- src/webservice.c
+++ src/webservice.c
@@ -1000,3 +1000,132 @@ int callback_glewlwyd_manage_plugin_module (const struct _u_request * request, s
   json_decref(j_search_module);
   return U_CALLBACK_CONTINUE;
 }
+
+int callback_glewlwyd_get_user_list (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  struct config_elements * config = (struct config_elements *)user_data;
+  json_t * j_user_list;
+  size_t offset = 0, limit = GLEWLWYD_DEFAULT_LIMIT_SIZE;
+  long int l_converted = 0;
+  char * endptr = NULL;
+  
+  if (u_map_get(request->map_url, "offset") != NULL) {
+    l_converted = strtol(u_map_get(request->map_url, "offset"), &endptr, 10);
+    if (!(*endptr) && l_converted > 0) {
+      offset = (size_t)l_converted;
+    }
+  }
+  if (u_map_get(request->map_url, "limit") != NULL) {
+    l_converted = strtol(u_map_get(request->map_url, "limit"), &endptr, 10);
+    if (!(*endptr) && l_converted > 0) {
+      limit = (size_t)l_converted;
+    }
+  }
+  j_user_list = get_user_list(config, u_map_get(request->map_url, "pattern"), offset, limit, u_map_get(request->map_url, "source"));
+  if (check_result_value(j_user_list, G_OK)) {
+    ulfius_set_json_body_response(response, 200, json_object_get(j_user_list, "user"));
+  } else {
+    y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_get_user_list - Error get_user_list");
+    response->status = 500;
+  }
+  json_decref(j_user_list);
+  return U_CALLBACK_CONTINUE;
+}
+
+int callback_glewlwyd_get_user (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  struct config_elements * config = (struct config_elements *)user_data;
+  json_t * j_user;
+  
+  j_user = get_user(config, u_map_get(request->map_url, "username"), u_map_get(request->map_url, "source"));
+  if (check_result_value(j_user, G_OK)) {
+    ulfius_set_json_body_response(response, 200, json_object_get(j_user, "user"));
+  } else if (check_result_value(j_user, G_ERROR_NOT_FOUND)) {
+    response->status = 404;
+  } else {
+    y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_get_user - Error j_user");
+    response->status = 500;
+  }
+  json_decref(j_user);
+  return U_CALLBACK_CONTINUE;
+}
+
+int callback_glewlwyd_add_user (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  struct config_elements * config = (struct config_elements *)user_data;
+  json_t * j_user, * j_user_valid;
+  
+  j_user = ulfius_get_json_body_request(request, NULL);
+  if (j_user != NULL) {
+    j_user_valid = is_user_valid(config, NULL, j_user, 1, u_map_get(request->map_url, "source"));
+    if (check_result_value(j_user_valid, G_OK)) {
+      if (add_user(config, j_user, u_map_get(request->map_url, "source")) != G_OK) {
+        y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_add_user - Error add_user");
+        response->status = 500;
+      }
+    } else if (check_result_value(j_user_valid, G_ERROR_PARAM)) {
+      ulfius_set_json_body_response(response, 400, json_object_get(j_user_valid, "user"));
+    } else {
+      y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_add_user - Error is_user_valid");
+      response->status = 500;
+    }
+    json_decref(j_user_valid);
+  } else {
+    response->status = 400;
+  }
+  json_decref(j_user);
+  return U_CALLBACK_CONTINUE;
+}
+
+int callback_glewlwyd_set_user (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  struct config_elements * config = (struct config_elements *)user_data;
+  json_t * j_user, * j_user_valid, * j_search_user;
+  
+  j_search_user = get_user(config, u_map_get(request->map_url, "username"), u_map_get(request->map_url, "source"));
+  if (check_result_value(j_search_user, G_OK)) {
+    j_user = ulfius_get_json_body_request(request, NULL);
+    if (j_user != NULL) {
+      j_user_valid = is_user_valid(config, u_map_get(request->map_url, "username"), j_user, 0, u_map_get(request->map_url, "source"));
+      if (check_result_value(j_user_valid, G_OK)) {
+        if (set_user(config, u_map_get(request->map_url, "username"), j_user, u_map_get(request->map_url, "source")) != G_OK) {
+          y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_set_user - Error set_user");
+          response->status = 500;
+        }
+      } else if (check_result_value(j_user_valid, G_ERROR_PARAM)) {
+        ulfius_set_json_body_response(response, 400, json_object_get(j_user_valid, "user"));
+      } else if (!check_result_value(j_user_valid, G_OK)) {
+        y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_set_user - Error is_user_valid");
+        response->status = 500;
+      }
+      json_decref(j_user_valid);
+    } else {
+      response->status = 400;
+    }
+    json_decref(j_user);
+  } else if (check_result_value(j_search_user, G_ERROR_NOT_FOUND)) {
+    response->status = 404;
+  } else {
+    y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_set_user - Error get_user");
+    response->status = 500;
+  }
+  json_decref(j_search_user);
+  return U_CALLBACK_CONTINUE;
+}
+
+int callback_glewlwyd_delete_user (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  struct config_elements * config = (struct config_elements *)user_data;
+  json_t * j_search_user;
+  
+  j_search_user = get_user(config, u_map_get(request->map_url, "username"), u_map_get(request->map_url, "source"));
+  if (check_result_value(j_search_user, G_OK)) {
+    if (delete_user(config, u_map_get(request->map_url, "username"), u_map_get(request->map_url, "source")) != G_OK) {
+      y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_delete_user - Error set_user");
+      response->status = 500;
+    }
+  } else if (check_result_value(j_search_user, G_ERROR_NOT_FOUND)) {
+    response->status = 404;
+  } else {
+    y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_delete_user - Error get_user");
+    response->status = 500;
+  }
+  json_decref(j_search_user);
+  return U_CALLBACK_CONTINUE;
+}
+
