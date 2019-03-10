e95fcddac6c03e36cbc889a375d0784aa101bf8f
babelouest <github@babelouest.org>
2019-03-09 22:01:10 -0500

    Set APIManagerthe way I want it to be, simple

--- webapp-src/src/lib/APIManager.js
+++ webapp-src/src/lib/APIManager.js
@@ -1,16 +1,34 @@
 class APIManager {
 	constructor() {
+    this.GlewlwydApiPrefix = "";
 	}
+
+  setConfig(GlewlwydApiPrefix) {
+    this.GlewlwydApiPrefix = GlewlwydApiPrefix;
+  }
 	
-	APIRequest(method, url, data) {
-		return $.ajax({
-			method: method,
-			url: url,
-			data: JSON.stringify(data),
-			contentType: data?"application/json; charset=utf-8":null
-		});
+	request(url, method="GET", data=false) {
+    if (data && method !== "GET") {
+  		return $.ajax({
+	  		method: method,
+		  	url: url,
+			  data: JSON.stringify(data),
+  			contentType: "application/json; charset=utf-8"
+	  	});
+    } else {
+      return $.ajax({
+        method: method,
+        url: url
+      });
+    }
 	}
+
+  glewlwydRequest(url, method="GET", data=false) {
+    return this.request(this.GlewlwydApiPrefix + url, method, data);
+  }
 	
 }
 
-export default APIManager;
+let apiManager = new APIManager();
+
+export default apiManager;
