9b8b5128fa2ebd18977202b94517a6c494e0d10f
babelouest <github@babelouest.org>
2019-03-19 20:32:55 -0400

    Check libconfig >= 1.4 in cmake script

--- cmake-modules/FindLibconfig.cmake
+++ cmake-modules/FindLibconfig.cmake
@@ -40,9 +40,27 @@ find_library(LIBCONFIG_LIBRARY
         NAMES libconfig config
         HINTS ${PC_LIBCONFIG_LIBDIR} ${PC_LIBCONFIG_LIBRARY_DIRS})
 
+if (PC_LIBCONFIG_VERSION)
+    set(LIBCONFIG_VERSION_STRING ${PC_LIBCONFIG_VERSION})
+elseif (LIBCONFIG_INCLUDE_DIR AND EXISTS "${LIBCONFIG_INCLUDE_DIR}/libconfig.h")
+    set(libconfig_version_list MAJOR MINOR REVISION)
+    foreach (v ${libconfig_version_list})
+        set(regex_libconfig_version "^#define LIBCONFIG_${v}_VERSION +\\(?([0-9]+)\\)?$")
+        file(STRINGS "${LIBCONFIG_INCLUDE_DIR}/libconfig.h" libconfig_version_${v} REGEX "${regex_libconfig_version}")
+        string(REGEX REPLACE "${regex_libconfig_version}" "\\1" libconfig_version_${v} "${libconfig_version_${v}}")
+        unset(regex_libconfig_version)
+    endforeach ()
+    set(LIBCONFIG_VERSION_STRING "${libconfig_version_MAJOR}.${libconfig_version_MINOR}.${libconfig_version_REVISION}")
+    foreach (v libconfig_version_list)
+        unset(libconfig_version_${v})
+    endforeach ()
+    unset(libconfig_version_list)
+endif ()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Libconfig
-        REQUIRED_VARS LIBCONFIG_LIBRARY LIBCONFIG_INCLUDE_DIR)
+        REQUIRED_VARS LIBCONFIG_LIBRARY LIBCONFIG_INCLUDE_DIR
+        VERSION_VAR LIBCONFIG_VERSION_STRING)
 
 if (LIBCONFIG_FOUND)
     set(LIBCONFIG_LIBRARIES ${LIBCONFIG_LIBRARY})
