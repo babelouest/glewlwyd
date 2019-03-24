ef2501c24d53f825cbb23240eb42d5866ce02c00
babelouest <github@babelouest.org>
2019-03-24 17:34:06 -0400

    Scheme can't be registered if username is different on body and session

--- src/webservice.c
+++ src/webservice.c
@@ -248,23 +248,27 @@ int callback_glewlwyd_user_auth_register (const struct _u_request * request, str
 
   if (j_param != NULL) {
     if (json_object_get(j_param, "username") != NULL && json_is_string(json_object_get(j_param, "username")) && json_string_length(json_object_get(j_param, "username"))) {
-      if (json_object_get(j_param, "scheme_type") != NULL && json_is_string(json_object_get(j_param, "scheme_type")) && json_string_length(json_object_get(j_param, "scheme_type")) && json_object_get(j_param, "scheme_name") != NULL && json_is_string(json_object_get(j_param, "scheme_name")) && json_string_length(json_object_get(j_param, "scheme_name"))) {
-        j_result = auth_register_user_scheme(config, json_string_value(json_object_get(j_param, "scheme_type")), json_string_value(json_object_get(j_param, "scheme_name")), json_string_value(json_object_get(j_param, "username")), json_object_get(j_param, "value"));
-        if (check_result_value(j_result, G_ERROR_PARAM)) {
-          ulfius_set_string_body_response(response, 400, "bad scheme parameters");
-        } else if (check_result_value(j_result, G_ERROR_NOT_FOUND)) {
-          response->status = 404;
-        } else if (check_result_value(j_result, G_ERROR_UNAUTHORIZED)) {
-          response->status = 401;
-        } else if (check_result_value(j_result, G_OK)) {
-          ulfius_set_json_body_response(response, 200, json_object_get(j_result, "register"));
+      if (0 == o_strcmp(json_string_value(json_object_get((json_t *)response->shared_data, "username")), json_string_value(json_object_get(j_param, "username")))) {
+        if (json_object_get(j_param, "scheme_type") != NULL && json_is_string(json_object_get(j_param, "scheme_type")) && json_string_length(json_object_get(j_param, "scheme_type")) && json_object_get(j_param, "scheme_name") != NULL && json_is_string(json_object_get(j_param, "scheme_name")) && json_string_length(json_object_get(j_param, "scheme_name"))) {
+          j_result = auth_register_user_scheme(config, json_string_value(json_object_get(j_param, "scheme_type")), json_string_value(json_object_get(j_param, "scheme_name")), json_string_value(json_object_get(j_param, "username")), json_object_get(j_param, "value"));
+          if (check_result_value(j_result, G_ERROR_PARAM)) {
+            ulfius_set_string_body_response(response, 400, "bad scheme parameters");
+          } else if (check_result_value(j_result, G_ERROR_NOT_FOUND)) {
+            response->status = 404;
+          } else if (check_result_value(j_result, G_ERROR_UNAUTHORIZED)) {
+            response->status = 401;
+          } else if (check_result_value(j_result, G_OK)) {
+            ulfius_set_json_body_response(response, 200, json_object_get(j_result, "register"));
+          } else {
+            y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_user_auth_register - Error auth_check_user_scheme");
+            response->status = 500;
+          }
+          json_decref(j_result);
         } else {
-          y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_user_auth_register - Error auth_check_user_scheme");
-          response->status = 500;
+          ulfius_set_string_body_response(response, 400, "scheme is mandatory");
         }
-        json_decref(j_result);
       } else {
-        ulfius_set_string_body_response(response, 400, "scheme is mandatory");
+        ulfius_set_string_body_response(response, 400, "username invalid");
       }
     } else {
       ulfius_set_string_body_response(response, 400, "username is mandatory");
