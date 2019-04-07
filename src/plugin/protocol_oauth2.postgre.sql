e71a01bc14c139472521faf98155ac523b5a555b
babelouest <github@babelouest.org>
2019-04-07 15:05:28 -0400

    Adapt postre sql scripts for postgresql, for real this time

--- src/plugin/protocol_oauth2.postgre.sql
+++ src/plugin/protocol_oauth2.postgre.sql
@@ -1,72 +1,72 @@
-DROP TABLE IF EXISTS `gpg_access_token_scope`;
-DROP TABLE IF EXISTS `gpg_access_token`;
-DROP TABLE IF EXISTS `gpg_refresh_token_scope`;
-DROP TABLE IF EXISTS `gpg_refresh_token`;
-DROP TABLE IF EXISTS `gpg_code_scope`;
-DROP TABLE IF EXISTS `gpg_code`;
+DROP TABLE IF EXISTS gpg_access_token_scope;
+DROP TABLE IF EXISTS gpg_access_token;
+DROP TABLE IF EXISTS gpg_refresh_token_scope;
+DROP TABLE IF EXISTS gpg_refresh_token;
+DROP TABLE IF EXISTS gpg_code_scope;
+DROP TABLE IF EXISTS gpg_code;
 
-CREATE TABLE `gpg_code` (
-  `gpgc_id` SERIAL PRIMARY KEY,
-  `gpgc_username` VARCHAR(256) NOT NULL,
-  `gpgc_client_id` VARCHAR(256) NOT NULL,
-  `gpgc_redirect_uri` VARCHAR(512) NOT NULL,
-  `gpgc_code_hash` VARCHAR(512) NOT NULL,
-  `gpgc_expires_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gpgc_issued_for` VARCHAR(256), -- IP address or hostname
-  `gpgc_user_agent` VARCHAR(256),
-  `gpgc_enabled` TINYINT(1) DEFAULT 1
+CREATE TABLE gpg_code (
+  gpgc_id SERIAL PRIMARY KEY,
+  gpgc_username VARCHAR(256) NOT NULL,
+  gpgc_client_id VARCHAR(256) NOT NULL,
+  gpgc_redirect_uri VARCHAR(512) NOT NULL,
+  gpgc_code_hash VARCHAR(512) NOT NULL,
+  gpgc_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpgc_issued_for VARCHAR(256), -- IP address or hostname
+  gpgc_user_agent VARCHAR(256),
+  gpgc_enabled SMALLINT DEFAULT 1
 );
-CREATE INDEX `i_gpgc_code_hash` ON `gpg_code`(`gpgc_code_hash`);
+CREATE INDEX i_gpgc_code_hash ON gpg_code(gpgc_code_hash);
 
-CREATE TABLE `gpg_code_scope` (
-  `gpgcs_id` SERIAL PRIMARY KEY,
-  `gpgc_id` SERIAL,
-  `gpgcs_scope` VARCHAR(128) NOT NULL,
-  FOREIGN KEY(`gpgc_id`) REFERENCES `gpg_code`(`gpgc_id`) ON DELETE CASCADE
+CREATE TABLE gpg_code_scope (
+  gpgcs_id SERIAL PRIMARY KEY,
+  gpgc_id INTEGER,
+  gpgcs_scope VARCHAR(128) NOT NULL,
+  FOREIGN KEY(gpgc_id) REFERENCES gpg_code(gpgc_id) ON DELETE CASCADE
 );
 
-CREATE TABLE `gpg_refresh_token` (
-  `gpgr_id` SERIAL PRIMARY KEY,
-  `gpgr_authorization_type` INT(2) NOT NULL, -- 0: Authorization Code Grant, 1: Implicit Grant, 2: Resource Owner Password Credentials Grant, 3: Client Credentials Grant
-  `gpgc_id` SERIAL DEFAULT NULL,
-  `gpgr_username` VARCHAR(256) NOT NULL,
-  `gpgr_client_id` VARCHAR(256),
-  `gpgr_issued_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gpgr_expires_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gpgr_last_seen` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gpgr_duration` SERIAL,
-  `gpgr_rolling_expiration` TINYINT(1) DEFAULT 0,
-  `gpgr_issued_for` VARCHAR(256), -- IP address or hostname
-  `gpgr_user_agent` VARCHAR(256),
-  `gpgr_token_hash` VARCHAR(512) NOT NULL,
-  `gpgr_enabled` TINYINT(1) DEFAULT 1,
-  FOREIGN KEY(`gpgc_id`) REFERENCES `gpg_code`(`gpgc_id`) ON DELETE CASCADE
+CREATE TABLE gpg_refresh_token (
+  gpgr_id SERIAL PRIMARY KEY,
+  gpgr_authorization_type SMALLINT NOT NULL, -- 0: Authorization Code Grant, 1: Implicit Grant, 2: Resource Owner Password Credentials Grant, 3: Client Credentials Grant
+  gpgc_id INTEGER DEFAULT NULL,
+  gpgr_username VARCHAR(256) NOT NULL,
+  gpgr_client_id VARCHAR(256),
+  gpgr_issued_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpgr_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpgr_last_seen TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpgr_duration INTEGER,
+  gpgr_rolling_expiration SMALLINT DEFAULT 0,
+  gpgr_issued_for VARCHAR(256), -- IP address or hostname
+  gpgr_user_agent VARCHAR(256),
+  gpgr_token_hash VARCHAR(512) NOT NULL,
+  gpgr_enabled SMALLINT DEFAULT 1,
+  FOREIGN KEY(gpgc_id) REFERENCES gpg_code(gpgc_id) ON DELETE CASCADE
 );
-CREATE INDEX `i_gpgr_token_hash` ON `gpg_refresh_token`(`gpgr_token_hash`);
+CREATE INDEX i_gpgr_token_hash ON gpg_refresh_token(gpgr_token_hash);
 
-CREATE TABLE `gpg_refresh_token_scope` (
-  `gpgrs_id` SERIAL PRIMARY KEY,
-  `gpgr_id` SERIAL,
-  `gpgrs_scope` VARCHAR(128) NOT NULL,
-  FOREIGN KEY(`gpgr_id`) REFERENCES `gpg_refresh_token`(`gpgr_id`) ON DELETE CASCADE
+CREATE TABLE gpg_refresh_token_scope (
+  gpgrs_id SERIAL PRIMARY KEY,
+  gpgr_id INTEGER,
+  gpgrs_scope VARCHAR(128) NOT NULL,
+  FOREIGN KEY(gpgr_id) REFERENCES gpg_refresh_token(gpgr_id) ON DELETE CASCADE
 );
 
 -- Access token table, to store meta information on access token sent
-CREATE TABLE `gpg_access_token` (
-  `gpga_id` SERIAL PRIMARY KEY,
-  `gpga_authorization_type` INT(2) NOT NULL, -- 0: Authorization Code Grant, 1: Implicit Grant, 2: Resource Owner Password Credentials Grant, 3: Client Credentials Grant
-  `gpgr_id` SERIAL DEFAULT NULL,
-  `gpga_username` VARCHAR(256),
-  `gpga_client_id` VARCHAR(256),
-  `gpga_issued_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `gpga_issued_for` VARCHAR(256), -- IP address or hostname
-  `gpga_user_agent` VARCHAR(256),
-  FOREIGN KEY(`gpgr_id`) REFERENCES `gpg_refresh_token`(`gpgr_id`) ON DELETE CASCADE
+CREATE TABLE gpg_access_token (
+  gpga_id SERIAL PRIMARY KEY,
+  gpga_authorization_type SMALLINT NOT NULL, -- 0: Authorization Code Grant, 1: Implicit Grant, 2: Resource Owner Password Credentials Grant, 3: Client Credentials Grant
+  gpgr_id INTEGER DEFAULT NULL,
+  gpga_username VARCHAR(256),
+  gpga_client_id VARCHAR(256),
+  gpga_issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gpga_issued_for VARCHAR(256), -- IP address or hostname
+  gpga_user_agent VARCHAR(256),
+  FOREIGN KEY(gpgr_id) REFERENCES gpg_refresh_token(gpgr_id) ON DELETE CASCADE
 );
 
-CREATE TABLE `gpg_access_token_scope` (
-  `gpgas_id` SERIAL PRIMARY KEY,
-  `gpga_id` SERIAL,
-  `gpgas_scope` VARCHAR(128) NOT NULL,
-  FOREIGN KEY(`gpga_id`) REFERENCES `gpg_access_token`(`gpga_id`) ON DELETE CASCADE
+CREATE TABLE gpg_access_token_scope (
+  gpgas_id SERIAL PRIMARY KEY,
+  gpga_id INTEGER,
+  gpgas_scope VARCHAR(128) NOT NULL,
+  FOREIGN KEY(gpga_id) REFERENCES gpg_access_token(gpga_id) ON DELETE CASCADE
 );
