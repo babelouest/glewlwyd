a581fa5011d5675e042930b56711232ba94a501b
babelouest <github@babelouest.org>
2019-04-11 18:12:04 -0400

    Allow glewlwyd_callback_check_session_valid to accept null scopes

--- src/plugin.c
+++ src/plugin.c
@@ -79,21 +79,25 @@ json_t * glewlwyd_callback_check_session_valid(struct config_plugin * config, co
   json_t * j_user, * j_return, * j_scope_allowed;
   char * session_uid = get_session_id(config->glewlwyd_config, request);
   
-  if (config != NULL && request != NULL && o_strlen(scope_list)) {
+  if (config != NULL && request != NULL) {
     j_user = get_current_user_for_session(config->glewlwyd_config, session_uid);
     // Check if session is valid
     if (check_result_value(j_user, G_OK)) {
-      // For all allowed scope, check that the current session has a valid session
-      j_scope_allowed = get_validated_auth_scheme_list_from_scope_list(config->glewlwyd_config, scope_list, session_uid);
-      if (check_result_value(j_scope_allowed, G_OK)) {
-        j_return = json_pack("{sis{sOsO}}", "result", G_OK, "session", "scope", json_object_get(j_scope_allowed, "scheme"), "user", json_object_get(j_user, "user"));
-      } else if (check_result_value(j_scope_allowed, G_ERROR_UNAUTHORIZED) || check_result_value(j_scope_allowed, G_ERROR_NOT_FOUND)) {
-        j_return = json_pack("{si}", "result", G_ERROR_UNAUTHORIZED);
+      if (o_strlen(scope_list)) {
+        // For all allowed scope, check that the current session has a valid session
+        j_scope_allowed = get_validated_auth_scheme_list_from_scope_list(config->glewlwyd_config, scope_list, session_uid);
+        if (check_result_value(j_scope_allowed, G_OK)) {
+          j_return = json_pack("{sis{sOsO}}", "result", G_OK, "session", "scope", json_object_get(j_scope_allowed, "scheme"), "user", json_object_get(j_user, "user"));
+        } else if (check_result_value(j_scope_allowed, G_ERROR_UNAUTHORIZED) || check_result_value(j_scope_allowed, G_ERROR_NOT_FOUND)) {
+          j_return = json_pack("{si}", "result", G_ERROR_UNAUTHORIZED);
+        } else {
+          y_log_message(Y_LOG_LEVEL_ERROR, "glewlwyd_callback_check_session_valid - Error get_validated_auth_scheme_list_from_scope_list");
+          j_return = json_pack("{si}", "result", G_ERROR);
+        }
+        json_decref(j_scope_allowed);
       } else {
-        y_log_message(Y_LOG_LEVEL_ERROR, "glewlwyd_callback_check_session_valid - Error get_validated_auth_scheme_list_from_scope_list");
-        j_return = json_pack("{si}", "result", G_ERROR);
+        j_return = json_pack("{sis{sO}}", "result", G_OK, "session", "user", json_object_get(j_user, "user"));
       }
-      json_decref(j_scope_allowed);
     } else if (check_result_value(j_user, G_ERROR_NOT_FOUND)) {
       j_return = json_pack("{si}", "result", G_ERROR_UNAUTHORIZED);
     } else {
