7d5df843300bb63b9bfa2b4d55f0a398fc8b2789
babelouest <github@babelouest.org>
2019-04-16 22:06:27 -0400

    Make notification a little bit more like a notification

--- webapp-src/src/lib/Notification.js
+++ webapp-src/src/lib/Notification.js
@@ -16,10 +16,16 @@ class Notification extends Component {
         this.setState({show: true, type: message.type, message: message.message}, () => {
           setTimeout(() => {
             this.setState({show: false});
-          }, 5000);
+          }, 50000);
         });
       }
     });
+    
+    this.close = this.close.bind(this);
+  }
+  
+  close() {
+    this.setState({show: false});
   }
 
   componentWillReceiveProps(nextProps) {
@@ -28,20 +34,20 @@ class Notification extends Component {
   render() {
     if (this.state.show) {
     return (
-    <div className="position-absolute w-100 d-flex flex-column p-4">
-      <div className={"toast fade show"} role="alert" aria-live="assertive" aria-atomic="true">
-        <div className="toast-header">
-          <svg className="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#007aff"></rect></svg>
-          <strong className="mr-auto">Glewlwyd</strong>
-          <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div className="toast-body">
-          {this.state.message}
+      <div style={{position: "absolute", top: 85, right: 5, borderWidth: "1px", borderStyle: "solid", borderRadius: "5px", padding: "5px"}}>
+        <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+          <div className="toast-header">
+            <svg className="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#007aff"></rect></svg>
+            <strong className="mr-auto">Glewlwyd</strong>
+            <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close" onClick={this.close}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="toast-body">
+            {this.state.message}
+          </div>
         </div>
       </div>
-    </div>
     );
     } else {
       return ("");
