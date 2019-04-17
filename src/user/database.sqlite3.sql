ac137f1aa4c32eb8b7b3f11f5bfb94b24554a457
babelouest <github@babelouest.org>
2019-04-17 13:46:54 -0400

    Remove backquotes

--- src/user/database.sqlite3.sql
+++ src/user/database.sqlite3.sql
@@ -1,35 +1,36 @@
-DROP TABLE IF EXISTS `g_user_property`;
-DROP TABLE IF EXISTS `g_user_scope_user`;
-DROP TABLE IF EXISTS `g_user_scope`;
-DROP TABLE IF EXISTS `g_user`;
+DROP TABLE IF EXISTS `g_client_property`;
+DROP TABLE IF EXISTS `g_client_scope_client`;
+DROP TABLE IF EXISTS `g_client_scope`;
+DROP TABLE IF EXISTS `g_client`;
 
-CREATE TABLE `g_user` (
-  `gu_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `gu_username` TEXT NOT NULL UNIQUE,
-  `gu_name` TEXT DEFAULT '',
-  `gu_email` TEXT,
-  `gu_password` TEXT,
-  `gu_enabled` INTEGER DEFAULT 1
+CREATE TABLE `g_client` (
+  `gc_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `gc_client_id` TEXT NOT NULL UNIQUE,
+  `gc_name` TEXT DEFAULT '',
+  `gc_description` TEXT DEFAULT '',
+  `gc_confidential` INTEGER DEFAULT 0,
+  `gc_password` TEXT,
+  `gc_enabled` INTEGER DEFAULT 1
 );
 
-CREATE TABLE `g_user_scope` (
-  `gus_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `gus_name` TEXT NOT NULL UNIQUE
+CREATE TABLE `g_client_scope` (
+  `gcs_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `gcs_name` TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE `g_user_scope_user` (
-  `gusu_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `gu_id` INTEGER,
-  `gus_id` INTEGER,
-  FOREIGN KEY(`gu_id`) REFERENCES `g_user`(`gu_id`) ON DELETE CASCADE,
-  FOREIGN KEY(`gus_id`) REFERENCES `g_user_scope`(`gus_id`) ON DELETE CASCADE
+CREATE TABLE `g_client_scope_client` (
+  `gcsu_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `gc_id` INTEGER,
+  `gcs_id` INTEGER,
+  FOREIGN KEY(`gc_id`) REFERENCES `g_client`(`gc_id`) ON DELETE CASCADE,
+  FOREIGN KEY(`gcs_id`) REFERENCES `g_client_scope`(`gcs_id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `g_user_property` (
-  `gup_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `gu_id` INTEGER,
-  `gup_name` TEXT NOT NULL,
-  `gup_value` TEXT DEFAULT NULL,
-  FOREIGN KEY(`gu_id`) REFERENCES `g_user`(`gu_id`) ON DELETE CASCADE
+CREATE TABLE `g_client_property` (
+  `gcp_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `gc_id` INTEGER,
+  `gcp_name` TEXT NOT NULL,
+  `gcp_value` TEXT DEFAULT NULL,
+  FOREIGN KEY(`gc_id`) REFERENCES `g_client`(`gc_id`) ON DELETE CASCADE
 );
-CREATE INDEX `i_g_user_property_name` ON `g_user_property`(`gup_name`);
+CREATE INDEX `i_g_client_property_name` ON `g_client_property`(`gcp_name`);
