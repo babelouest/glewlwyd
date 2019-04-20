b59b16ea22119b9ace1db4948b8c3f3335b791c3
babelouest <github@babelouest.org>
2019-04-20 09:51:43 -0400

    oauth2_irl test prepared, just the authentication workflow to make

---
+++ test/glewlwyd_oauth2_irl.c
@@ -0,0 +1,186 @@
+/* Public domain, no copyright. Use at your own risk. */
+
+/**
+ *
+ * This test is used to validate one user backend module that will be created upon start and deleted after
+ * The user backend must be in write mode
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+
+#include <check.h>
+#include <ulfius.h>
+#include <orcania.h>
+#include <yder.h>
+
+#include "unit-tests.h"
+#include "../src/glewlwyd-common.h"
+
+#define SERVER_URI "http://localhost:4593/api"
+#define ADMIN_USERNAME "admin"
+#define ADMIN_PASSWORD "password"
+#define MOD_NAME "mod_irl"
+#define PROFILE_SCOPE_1 "g_profile"
+#define PROFILE_SCOPE_2 "scope1"
+
+struct _u_request admin_req;
+json_t * j_params;
+
+START_TEST(test_glwd_oauth2_irl_user_module_add)
+{
+  char * url = SERVER_URI "/mod/user";
+  ck_assert_int_eq(run_simple_test(&admin_req, "POST", url, NULL, NULL, json_object_get(j_params, "user_mod"), NULL, 200, NULL, NULL, NULL), 1);
+}
+END_TEST
+
+START_TEST(test_glwd_oauth2_irl_client_module_add)
+{
+  char * url = SERVER_URI "/mod/client";
+  ck_assert_int_eq(run_simple_test(&admin_req, "POST", url, NULL, NULL, json_object_get(j_params, "client_mod"), NULL, 200, NULL, NULL, NULL), 1);
+}
+END_TEST
+
+START_TEST(test_glwd_oauth2_irl_user_add)
+{
+  if (json_object_get(j_params, "user_add") == json_true()) {
+    char * url = msprintf(SERVER_URI "/user?source=%s", json_string_value(json_object_get(json_object_get(j_params, "user_mod"), "name")));
+    ck_assert_int_eq(run_simple_test(&admin_req, "POST", url, NULL, NULL, json_object_get(j_params, "user"), NULL, 200, NULL, NULL, NULL), 1);
+    o_free(url);
+  }
+}
+END_TEST
+
+START_TEST(test_glwd_oauth2_irl_client_add)
+{
+  if (json_object_get(j_params, "client_add") == json_true()) {
+    char * url = msprintf(SERVER_URI "/client?source=%s", json_string_value(json_object_get(json_object_get(j_params, "client_mod"), "name")));
+    ck_assert_int_eq(run_simple_test(&admin_req, "POST", url, NULL, NULL, json_object_get(j_params, "client"), NULL, 200, NULL, NULL, NULL), 1);
+    o_free(url);
+  }
+}
+END_TEST
+
+START_TEST(test_glwd_oauth2_irl_run_workflow)
+{
+}
+END_TEST
+
+START_TEST(test_glwd_oauth2_irl_user_delete)
+{
+  if (json_object_get(j_params, "user_add") == json_true()) {
+    char * url = msprintf(SERVER_URI "/user/%s?source=%s", json_string_value(json_object_get(json_object_get(j_params, "user"), "username")), json_string_value(json_object_get(json_object_get(j_params, "user_mod"), "name")));
+    ck_assert_int_eq(run_simple_test(&admin_req, "DELETE", url, NULL, NULL, NULL, NULL, 200, NULL, NULL, NULL), 1);
+    o_free(url);
+  }
+}
+END_TEST
+
+START_TEST(test_glwd_oauth2_irl_client_delete)
+{
+  if (json_object_get(j_params, "user_add") == json_true()) {
+    char * url = msprintf(SERVER_URI "/client/%s?source=%s", json_string_value(json_object_get(json_object_get(j_params, "client"), "client_id")), json_string_value(json_object_get(json_object_get(j_params, "client_mod"), "name")));
+    ck_assert_int_eq(run_simple_test(&admin_req, "DELETE", url, NULL, NULL, NULL, NULL, 200, NULL, NULL, NULL), 1);
+    o_free(url);
+  }
+}
+END_TEST
+
+START_TEST(test_glwd_oauth2_irl_user_module_delete)
+{
+  char * url = msprintf(SERVER_URI "/mod/user/%s", json_string_value(json_object_get(json_object_get(j_params, "user_mod"), "name")));
+  ck_assert_int_eq(run_simple_test(&admin_req, "DELETE", url, NULL, NULL, NULL, NULL, 200, NULL, NULL, NULL), 1);
+}
+END_TEST
+
+START_TEST(test_glwd_oauth2_irl_client_module_delete)
+{
+  char * url = msprintf(SERVER_URI "/mod/client/%s", json_string_value(json_object_get(json_object_get(j_params, "client_mod"), "name")));
+  ck_assert_int_eq(run_simple_test(&admin_req, "DELETE", url, NULL, NULL, NULL, NULL, 200, NULL, NULL, NULL), 1);
+}
+END_TEST
+
+static Suite *glewlwyd_suite(void)
+{
+  Suite *s;
+  TCase *tc_core;
+
+  s = suite_create("Glewlwyd oauth2 irl");
+  tc_core = tcase_create("test_glwd_oauth2_irl");
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_user_module_add);
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_client_module_add);
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_user_add);
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_client_add);
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_run_workflow);
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_user_delete);
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_client_delete);
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_user_module_delete);
+  tcase_add_test(tc_core, test_glwd_oauth2_irl_client_module_delete);
+  tcase_set_timeout(tc_core, 90);
+  suite_add_tcase(s, tc_core);
+
+  return s;
+}
+
+int main(int argc, char *argv[])
+{
+  int number_failed = 0;
+  Suite *s;
+  SRunner *sr;
+  struct _u_request auth_req;
+  struct _u_response auth_resp;
+  int res, do_test = 0;
+  json_t * j_body;
+  char * cookie;
+  
+  y_init_logs("Glewlwyd test", Y_LOG_MODE_CONSOLE, Y_LOG_LEVEL_DEBUG, NULL, "Starting Glewlwyd test");
+  
+  srand(time(NULL));
+  j_params = json_load_file(argv[1], JSON_DECODE_ANY, NULL);
+  ulfius_init_request(&admin_req);
+  if (j_params != NULL) {
+    // Getting a valid session id for authenticated http requests
+    ulfius_init_request(&auth_req);
+    ulfius_init_response(&auth_resp);
+    auth_req.http_verb = strdup("POST");
+    auth_req.http_url = msprintf("%s/auth/", SERVER_URI);
+    j_body = json_pack("{ssss}", "username", ADMIN_USERNAME, "password", ADMIN_PASSWORD);
+    ulfius_set_json_body_request(&auth_req, j_body);
+    json_decref(j_body);
+    res = ulfius_send_http_request(&auth_req, &auth_resp);
+    if (res == U_OK && auth_resp.status == 200) {
+      if (auth_resp.nb_cookies) {
+        cookie = msprintf("%s=%s", auth_resp.map_cookie[0].key, auth_resp.map_cookie[0].value);
+        u_map_put(admin_req.map_header, "Cookie", cookie);
+        o_free(cookie);
+        do_test = 1;
+      }
+    } else {
+      y_log_message(Y_LOG_LEVEL_ERROR, "Error authentication");
+    }
+    ulfius_clean_response(&auth_resp);
+    ulfius_clean_request(&auth_req);
+    
+    if (do_test) {
+      s = glewlwyd_suite();
+      sr = srunner_create(s);
+
+      srunner_run_all(sr, CK_VERBOSE);
+      number_failed = srunner_ntests_failed(sr);
+      srunner_free(sr);
+    }
+    
+  } else {
+    y_log_message(Y_LOG_LEVEL_ERROR, "Error reading parameters file %s", argv[1]);
+  }
+  json_decref(j_params);
+  ulfius_clean_request(&admin_req);
+  y_close_logs();
+  
+  return (do_test && number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
