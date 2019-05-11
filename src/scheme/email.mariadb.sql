7e8a27f5fb52316777ed1479f681da7f8d8c6faf
babelouest <github@babelouest.org>
2019-05-10 20:24:46 -0400

    Fix email scheme sql scripts to remove backquotes

--- src/scheme/email.mariadb.sql
+++ src/scheme/email.mariadb.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS `gs_code`;
+DROP TABLE IF EXISTS gs_code;
 
-CREATE TABLE `gs_code` (
-  `gsc_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
-  `gsc_issued_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gsc_username` VARCHAR(128) NOT NULL,
-  `gsc_enabled` TINYINT(1) DEFAULT 1,
-  `gsc_code_hash` VARCHAR(128),
-  `gsc_result` TINYINT(1) DEFAULT 0
+CREATE TABLE gs_code (
+  gsc_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  gsc_issued_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gsc_username VARCHAR(128) NOT NULL,
+  gsc_enabled TINYINT(1) DEFAULT 1,
+  gsc_code_hash VARCHAR(128),
+  gsc_result TINYINT(1) DEFAULT 0
 );
-CREATE INDEX `i_gssc_username` ON `gs_code`(`gsc_username`);
+CREATE INDEX i_gssc_username ON gs_code(gsc_username);
--- src/scheme/email.postgre.sql
+++ src/scheme/email.postgre.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS `gss_code`;
+DROP TABLE IF EXISTS gss_code;
 
-CREATE TABLE `gs_code` (
-  `gsc_id` SERIAL PRIMARY KEY,
-  `gsc_issued_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `gsc_username` VARCHAR(128) NOT NULL,
-  `gsc_enabled` SMALLINT DEFAULT 1,
-  `gsc_code_hash` VARCHAR(128),
-  `gsc_result` SMALLINT DEFAULT 0
+CREATE TABLE gs_code (
+  gsc_id SERIAL PRIMARY KEY,
+  gsc_issued_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  gsc_username VARCHAR(128) NOT NULL,
+  gsc_enabled SMALLINT DEFAULT 1,
+  gsc_code_hash VARCHAR(128),
+  gsc_result SMALLINT DEFAULT 0
 );
-CREATE INDEX `i_gsc_username` ON `gs_code`(`gsc_username`);
+CREATE INDEX i_gsc_username ON gs_code(gsc_username);
