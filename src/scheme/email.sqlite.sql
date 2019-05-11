37d698158239b4a31f3737ee5de395b5a02e1142
babelouest <github@babelouest.org>
2019-05-10 20:24:46 -0400

    Fix email scheme sql scripts to remove backquotes

--- src/scheme/email.sqlite.sql
+++ src/scheme/email.sqlite.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS `gs_code`;
+DROP TABLE IF EXISTS gs_code;
 
-CREATE TABLE `gs_code` (
-  `gsc_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `gsc_issued_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gsc_username` TEXT NOT NULL,
-  `gsc_enabled` INTEGER DEFAULT 1,
-  `gsc_code_hash` TEXT,
-  `gsc_result` INTEGER DEFAULT 0
+CREATE TABLE gs_code (
+  gsc_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gsc_issued_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gsc_username TEXT NOT NULL,
+  gsc_enabled INTEGER DEFAULT 1,
+  gsc_code_hash TEXT,
+  gsc_result INTEGER DEFAULT 0
 );
-CREATE INDEX `i_gsc_username` ON `gs_code`(`gsc_username`);
+CREATE INDEX i_gsc_username ON gs_code(gsc_username);
