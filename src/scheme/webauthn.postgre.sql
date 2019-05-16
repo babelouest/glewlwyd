c9c4b4f5b6dbb8b0c7a42b7b31fd9e4c81421e59
babelouest <github@babelouest.org>
2019-05-15 20:50:16 -0400

    Add gs_webauthn_assertion and indexes

--- src/scheme/webauthn.postgre.sql
+++ src/scheme/webauthn.postgre.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS gs_webauthn_assertion;
 DROP TABLE IF EXISTS gs_webauthn_credential;
 DROP TABLE IF EXISTS gs_webauthn_user;
 
@@ -17,8 +18,20 @@ CREATE TABLE gs_webauthn_credential (
   gswc_credential_id VARCHAR(256),
   gswc_public_key TEXT DEFAULT NULL,
   gswc_created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  gswc_last_seen TIMESTAMP,
   gswc_status SMALLINT DEFAULT 0, -- 0 new, 1 registered, 2 error, 3 closed, 4 cancelled
   FOREIGN KEY(gswu_id) REFERENCES gs_webauthn_user(gswu_id) ON DELETE CASCADE
 );
-CREATE INDEX i_gswc_credential_id ON gs_webauthn_credential(gswc_credential_id);
+CREATE INDEX i_gswc_public_key ON gs_webauthn_credential(gswc_public_key);
+CREATE INDEX i_gswc_session_hash ON gs_webauthn_credential(gswc_session_hash);
+
+CREATE TABLE gs_webauthn_assertion (
+  gswa_id SERIAL PRIMARY KEY,
+  gswu_id INTEGER NOT NULL,
+  gswc_id INTEGER,
+  gswa_session_hash VARCHAR(128) NOT NULL,
+  gswa_challenge_hash VARCHAR(128),
+  gswa_status SMALLINT DEFAULT 0, -- 0 new, 1 verified, 2 not verified, 3 error
+  FOREIGN KEY(gswu_id) REFERENCES gs_webauthn_user(gswu_id) ON DELETE CASCADE,
+  FOREIGN KEY(gswc_id) REFERENCES gs_webauthn_credential(gswc_id) ON DELETE CASCADE
+);
+CREATE INDEX i_gswa_session_hash ON gs_webauthn_assertion(gswa_session_hash);
--- src/scheme/webauthn.sqlite.sql
+++ src/scheme/webauthn.sqlite.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS gs_webauthn_assertion;
 DROP TABLE IF EXISTS gs_webauthn_credential;
 DROP TABLE IF EXISTS gs_webauthn_user;
 
@@ -17,8 +18,20 @@ CREATE TABLE gs_webauthn_credential (
   gswc_credential_id TEXT,
   gswc_public_key BLOB DEFAULT NULL,
   gswc_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  gswc_last_seen TIMESTAMP,
   gswc_status INTEGER DEFAULT 0, -- 0 new, 1 registered, 2 error, 3 closed, 4 cancelled
   FOREIGN KEY(gswu_id) REFERENCES gs_webauthn_user(gswu_id) ON DELETE CASCADE
 );
-CREATE INDEX i_gswc_credential_id ON gs_webauthn_credential(gswc_credential_id);
+CREATE INDEX i_gswc_public_key ON gs_webauthn_credential(gswc_public_key);
+CREATE INDEX i_gswc_session_hash ON gs_webauthn_credential(gswc_session_hash);
+
+CREATE TABLE gs_webauthn_assertion (
+  gswa_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gswu_id INTEGER NOT NULL,
+  gswc_id INTEGER,
+  gswa_session_hash TEXT NOT NULL,
+  gswa_challenge_hash TEXT,
+  gswa_status SMALLINT DEFAULT 0, -- 0 new, 1 verified, 2 not verified, 3 error
+  FOREIGN KEY(gswu_id) REFERENCES gs_webauthn_user(gswu_id) ON DELETE CASCADE,
+  FOREIGN KEY(gswc_id) REFERENCES gs_webauthn_credential(gswc_id) ON DELETE CASCADE
+);
+CREATE INDEX i_gswa_session_hash ON gs_webauthn_assertion(gswa_session_hash);
