fe49ad79388c527e8cff9f9725c29a5ab6c239c6
babelouest <github@babelouest.org>
2019-05-15 20:23:36 -0400

    Add gswc_name and gswc_last_seen

--- src/scheme/webauthn.mariadb.sql
+++ src/scheme/webauthn.mariadb.sql
@@ -12,10 +12,12 @@ CREATE TABLE gs_webauthn_credential (
   gswc_id INT(11) PRIMARY KEY AUTO_INCREMENT,
   gswu_id INT(11) NOT NULL,
   gswc_session_hash VARCHAR(128) NOT NULL,
+  gswc_name VARCHAR(128),
   gswc_challenge_hash VARCHAR(128),
-  gswc_credential_id VARCHAR(128),
+  gswc_credential_id VARCHAR(256),
   gswc_public_key BLOB DEFAULT NULL,
   gswc_created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gswc_last_seen TIMESTAMP,
   gswc_status TINYINT(1) DEFAULT 0, -- 0 new, 1 registered, 2 error, 3 closed, 4 cancelled
   FOREIGN KEY(gswu_id) REFERENCES gs_webauthn_user(gswu_id) ON DELETE CASCADE
 );
