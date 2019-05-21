66c339512c8321ea42d71316e965d1f749ef14f0
babelouest <github@babelouest.org>
2019-05-20 20:27:44 -0400

    Implement webauthn scheme function user_auth_scheme_module_can_use

--- src/scheme/webauthn.c
+++ src/scheme/webauthn.c
@@ -1660,7 +1660,29 @@ int user_auth_scheme_module_close(struct config_module * config, void * cls) {
  * 
  */
 int user_auth_scheme_module_can_use(struct config_module * config, const char * username, void * cls) {
-  return GLEWLWYD_IS_AVAILABLE;
+  json_t * j_user_id, * j_credential;
+  int ret;
+  
+  j_user_id = get_user_id_from_username(config, username, 0);
+  if (check_result_value(j_user_id, G_OK)) {
+    j_credential = get_credential_list(config, username, 1);
+    if (check_result_value(j_credential, G_OK)) {
+      ret = GLEWLWYD_IS_REGISTERED;
+    } else if (check_result_value(j_credential, G_ERROR_NOT_FOUND)) {
+      ret = GLEWLWYD_IS_AVAILABLE;
+    } else {
+      y_log_message(Y_LOG_LEVEL_ERROR, "user_auth_scheme_module_can_use webauthn - Error get_credential_list");
+      ret = GLEWLWYD_IS_NOT_AVAILABLE;
+    }
+    json_decref(j_credential);
+  } else if (check_result_value(j_user_id, G_ERROR_NOT_FOUND)) {
+    ret = GLEWLWYD_IS_AVAILABLE;
+  } else {
+    y_log_message(Y_LOG_LEVEL_ERROR, "user_auth_scheme_module_can_use webauthn - Error get_user_id_from_username");
+    ret = GLEWLWYD_IS_NOT_AVAILABLE;
+  }
+  json_decref(j_user_id);
+  return ret;
 }
 
 /**
