1a3d4ffa7c8f4c150bc8466ee194947826c45a46
babelouest <github@babelouest.org>
2019-05-28 15:12:57 -0400

    Complete UI for scheme module OTP

--- webapp-src/src/Admin/ModEdit.js
+++ webapp-src/src/Admin/ModEdit.js
@@ -167,7 +167,7 @@ class ModEdit extends Component {
     }
 		return (
     <div className="modal fade" id="editModModal" tabIndex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
-      <div className="modal-dialog" role="document">
+      <div className="modal-dialog modal-lg" role="document">
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title" id="confirmModalLabel">{this.state.title}</h5>
