96999a7d6b2c723c38c374afa1426c0d27d8d1b0
babelouest <github@babelouest.org>
2019-07-23 22:00:24 -0400

    Webauthn: Show modal message when an error occured and the error message may be relevant fot the user

---
+++ webapp-src/src/Modal/Message.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: props.title,
+      message: props.message
+    }
+
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      title: nextProps.title,
+      message: nextProps.message
+    });
+  }
+
+  closeModal(e, result) {
+    $("#messageModal").modal("hide");
+  }
+  
+	render() {
+    var messageJsx = [];
+    if (this.state.message) {
+      this.state.message.forEach((message, index) => {
+        messageJsx.push(<li key={index}>{message}</li>);
+      });
+    }
+		return (
+    <div className="modal fade" id="messageModal" tabIndex="-1" role="dialog" aria-labelledby="messageModalLabel" aria-hidden="true">
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="messageModalLabel">{this.state.title}</h5>
+            <button type="button" className="close" aria-label={i18next.t("modal.close")} onClick={(e) => this.closeModal(e, false)}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <ul>
+              {messageJsx}
+            </ul>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" onClick={(e) => this.closeModal(e, false)}>{i18next.t("modal.close")}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+		);
+	}
+}
+
+export default Message;
