845444b57f8b66d2a4983d57beff67a630dd4084
babelouest <github@babelouest.org>
2019-07-30 16:29:27 -0400

    Insert oidc tables in default database tables creation

--- docs/database/init.postgre.sql
+++ docs/database/init.postgre.sql
@@ -28,7 +28,16 @@ DROP TABLE IF EXISTS gpg_refresh_token_scope;
 DROP TABLE IF EXISTS gpg_refresh_token;
 DROP TABLE IF EXISTS gpg_code_scope;
 DROP TABLE IF EXISTS gpg_code;
-DROP TABLE IF EXISTS gss_code;
+DROP TABLE IF EXISTS gpo_id_token_scope;
+DROP TABLE IF EXISTS gpo_id_token;
+DROP TABLE IF EXISTS gpo_access_token_scope;
+DROP TABLE IF EXISTS gpo_access_token;
+DROP TABLE IF EXISTS gpo_refresh_token_scope;
+DROP TABLE IF EXISTS gpo_refresh_token;
+DROP TABLE IF EXISTS gpo_code_scheme;
+DROP TABLE IF EXISTS gpo_code_scope;
+DROP TABLE IF EXISTS gpo_code;
+DROP TABLE IF EXISTS gs_code;
 DROP TABLE IF EXISTS gs_webauthn_assertion;
 DROP TABLE IF EXISTS gs_webauthn_credential;
 DROP TABLE IF EXISTS gs_webauthn_user;
@@ -265,6 +274,93 @@ CREATE TABLE gpg_access_token_scope (
   gpga_id INTEGER,
   gpgas_scope VARCHAR(128) NOT NULL,
   FOREIGN KEY(gpga_id) REFERENCES gpg_access_token(gpga_id) ON DELETE CASCADE
+);
+
+CREATE TABLE gpo_code (
+  gpoc_id SERIAL PRIMARY KEY,
+  gpoc_authorization_type SMALLINT NOT NULL,
+  gpoc_username VARCHAR(256) NOT NULL,
+  gpoc_client_id VARCHAR(256) NOT NULL,
+  gpoc_redirect_uri VARCHAR(512) NOT NULL,
+  gpoc_code_hash VARCHAR(512) NOT NULL,
+  gpoc_nonce VARCHAR(512),
+  gpoc_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpoc_issued_for VARCHAR(256), -- IP address or hostname
+  gpoc_user_agent VARCHAR(256),
+  gpoc_enabled SMALLINT DEFAULT 1
+);
+CREATE INDEX i_gpoc_code_hash ON gpo_code(gpoc_code_hash);
+
+CREATE TABLE gpo_code_scope (
+  gpocs_id SERIAL PRIMARY KEY,
+  gpoc_id INTEGER,
+  gpocs_scope VARCHAR(128) NOT NULL,
+  FOREIGN KEY(gpoc_id) REFERENCES gpo_code(gpoc_id) ON DELETE CASCADE
+);
+
+CREATE TABLE gpo_code_scheme (
+  gpoch_id SERIAL PRIMARY KEY,
+  gpoc_id INT(11),
+  gpoch_scheme_module VARCHAR(128) NOT NULL,
+  FOREIGN KEY(gpoc_id) REFERENCES gpo_code(gpoc_id) ON DELETE CASCADE
+);
+
+CREATE TABLE gpo_refresh_token (
+  gpor_id SERIAL PRIMARY KEY,
+  gpor_authorization_type SMALLINT NOT NULL,
+  gpoc_id INTEGER DEFAULT NULL,
+  gpor_username VARCHAR(256) NOT NULL,
+  gpor_client_id VARCHAR(256),
+  gpor_issued_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpor_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpor_last_seen TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpor_duration INTEGER,
+  gpor_rolling_expiration SMALLINT DEFAULT 0,
+  gpor_issued_for VARCHAR(256), -- IP address or hostname
+  gpor_user_agent VARCHAR(256),
+  gpor_token_hash VARCHAR(512) NOT NULL,
+  gpor_enabled SMALLINT DEFAULT 1,
+  FOREIGN KEY(gpoc_id) REFERENCES gpo_code(gpoc_id) ON DELETE CASCADE
+);
+CREATE INDEX i_gpor_token_hash ON gpo_refresh_token(gpor_token_hash);
+
+CREATE TABLE gpo_refresh_token_scope (
+  gpors_id SERIAL PRIMARY KEY,
+  gpor_id INTEGER,
+  gpors_scope VARCHAR(128) NOT NULL,
+  FOREIGN KEY(gpor_id) REFERENCES gpo_refresh_token(gpor_id) ON DELETE CASCADE
+);
+
+-- Access token table, to store meta information on access token sent
+CREATE TABLE gpo_access_token (
+  gpoa_id SERIAL PRIMARY KEY,
+  gpoa_authorization_type SMALLINT NOT NULL,
+  gpor_id INTEGER DEFAULT NULL,
+  gpoa_username VARCHAR(256),
+  gpoa_client_id VARCHAR(256),
+  gpoa_issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gpoa_issued_for VARCHAR(256), -- IP address or hostname
+  gpoa_user_agent VARCHAR(256),
+  FOREIGN KEY(gpor_id) REFERENCES gpo_refresh_token(gpor_id) ON DELETE CASCADE
+);
+
+CREATE TABLE gpo_access_token_scope (
+  gpoas_id SERIAL PRIMARY KEY,
+  gpoa_id INTEGER,
+  gpoas_scope VARCHAR(128) NOT NULL,
+  FOREIGN KEY(gpoa_id) REFERENCES gpo_access_token(gpoa_id) ON DELETE CASCADE
+);
+
+-- Id token table, to store meta information on id token sent
+CREATE TABLE gpo_id_token (
+  gpoi_id SERIAL PRIMARY KEY,
+  gpoi_authorization_type SMALLINT NOT NULL,
+  gpoi_username VARCHAR(256),
+  gpoi_client_id VARCHAR(256),
+  gpoi_issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gpoi_issued_for VARCHAR(256), -- IP address or hostname
+  gpoi_user_agent VARCHAR(256),
+  gpoi_hash VARCHAR(512)
 );
 
 CREATE TABLE gs_code (
--- docs/database/init.sqlite3.sql
+++ docs/database/init.sqlite3.sql
@@ -14,12 +14,6 @@ DROP TABLE IF EXISTS g_user_module_instance;
 DROP TABLE IF EXISTS g_user_auth_scheme_module_instance;
 DROP TABLE IF EXISTS g_client_module_instance;
 DROP TABLE IF EXISTS g_user_session;
-DROP TABLE IF EXISTS gpg_access_token_scope;
-DROP TABLE IF EXISTS gpg_access_token;
-DROP TABLE IF EXISTS gpg_refresh_token_scope;
-DROP TABLE IF EXISTS gpg_refresh_token;
-DROP TABLE IF EXISTS gpg_code_scope;
-DROP TABLE IF EXISTS gpg_code;
 DROP TABLE IF EXISTS g_client_property;
 DROP TABLE IF EXISTS g_client_scope_client;
 DROP TABLE IF EXISTS g_client_scope;
@@ -28,6 +22,21 @@ DROP TABLE IF EXISTS g_user_property;
 DROP TABLE IF EXISTS g_user_scope_user;
 DROP TABLE IF EXISTS g_user_scope;
 DROP TABLE IF EXISTS g_user;
+DROP TABLE IF EXISTS gpg_access_token_scope;
+DROP TABLE IF EXISTS gpg_access_token;
+DROP TABLE IF EXISTS gpg_refresh_token_scope;
+DROP TABLE IF EXISTS gpg_refresh_token;
+DROP TABLE IF EXISTS gpg_code_scope;
+DROP TABLE IF EXISTS gpg_code;
+DROP TABLE IF EXISTS gpo_id_token_scope;
+DROP TABLE IF EXISTS gpo_id_token;
+DROP TABLE IF EXISTS gpo_access_token_scope;
+DROP TABLE IF EXISTS gpo_access_token;
+DROP TABLE IF EXISTS gpo_refresh_token_scope;
+DROP TABLE IF EXISTS gpo_refresh_token;
+DROP TABLE IF EXISTS gpo_code_scheme;
+DROP TABLE IF EXISTS gpo_code_scope;
+DROP TABLE IF EXISTS gpo_code;
 DROP TABLE IF EXISTS gs_code;
 DROP TABLE IF EXISTS gs_webauthn_assertion;
 DROP TABLE IF EXISTS gs_webauthn_credential;
@@ -265,6 +274,93 @@ CREATE TABLE gpg_access_token_scope (
   gpga_id INT(11),
   gpgas_scope TEXT NOT NULL,
   FOREIGN KEY(gpga_id) REFERENCES gpg_access_token(gpga_id) ON DELETE CASCADE
+);
+
+CREATE TABLE gpo_code (
+  gpoc_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpoc_authorization_type INTEGER NOT NULL,
+  gpoc_username TEXT NOT NULL,
+  gpoc_client_id TEXT NOT NULL,
+  gpoc_redirect_uri TEXT NOT NULL,
+  gpoc_code_hash TEXT NOT NULL,
+  gpoc_nonce TEXT,
+  gpoc_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpoc_issued_for TEXT, -- IP address or hostname
+  gpoc_user_agent TEXT,
+  gpoc_enabled INTEGER DEFAULT 1
+);
+CREATE INDEX i_gpoc_code_hash ON gpo_code(gpoc_code_hash);
+
+CREATE TABLE gpo_code_scope (
+  gpocs_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpoc_id INTEGER,
+  gpocs_scope TEXT NOT NULL,
+  FOREIGN KEY(gpoc_id) REFERENCES gpo_code(gpoc_id) ON DELETE CASCADE
+);
+
+CREATE TABLE gpo_code_scheme (
+  gpoch_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpoc_id INTEGER,
+  gpoch_scheme_module TEXT NOT NULL,
+  FOREIGN KEY(gpoc_id) REFERENCES gpo_code(gpoc_id) ON DELETE CASCADE
+);
+
+CREATE TABLE gpo_refresh_token (
+  gpor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpor_authorization_type INTEGER NOT NULL,
+  gpoc_id INTEGER DEFAULT NULL,
+  gpor_username TEXT NOT NULL,
+  gpor_client_id TEXT,
+  gpor_issued_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpor_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpor_last_seen TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gpor_duration INTEGER,
+  gpor_rolling_expiration INTEGER DEFAULT 0,
+  gpor_issued_for TEXT, -- IP address or hostname
+  gpor_user_agent TEXT,
+  gpor_token_hash TEXT NOT NULL,
+  gpor_enabled INTEGER DEFAULT 1,
+  FOREIGN KEY(gpoc_id) REFERENCES gpo_code(gpoc_id) ON DELETE CASCADE
+);
+CREATE INDEX i_gpor_token_hash ON gpo_refresh_token(gpor_token_hash);
+
+CREATE TABLE gpo_refresh_token_scope (
+  gpors_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpor_id INTEGER,
+  gpors_scope TEXT NOT NULL,
+  FOREIGN KEY(gpor_id) REFERENCES gpo_refresh_token(gpor_id) ON DELETE CASCADE
+);
+
+-- Access token table, to store meta information on access token sent
+CREATE TABLE gpo_access_token (
+  gpoa_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpoa_authorization_type INTEGER NOT NULL,
+  gpor_id INTEGER DEFAULT NULL,
+  gpoa_username TEXT,
+  gpoa_client_id TEXT,
+  gpoa_issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gpoa_issued_for TEXT, -- IP address or hostname
+  gpoa_user_agent TEXT,
+  FOREIGN KEY(gpor_id) REFERENCES gpo_refresh_token(gpor_id) ON DELETE CASCADE
+);
+
+CREATE TABLE gpo_access_token_scope (
+  gpoas_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpoa_id INT(11),
+  gpoas_scope TEXT NOT NULL,
+  FOREIGN KEY(gpoa_id) REFERENCES gpo_access_token(gpoa_id) ON DELETE CASCADE
+);
+
+-- Id token table, to store meta information on id token sent
+CREATE TABLE gpo_id_token (
+  gpoi_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpoi_authorization_type INTEGER NOT NULL,
+  gpoi_username TEXT,
+  gpoi_client_id TEXT,
+  gpoi_issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gpoi_issued_for TEXT, -- IP address or hostname
+  gpoi_user_agent TEXT,
+  gpoi_hash TEXT
 );
 
 CREATE TABLE gs_code (
