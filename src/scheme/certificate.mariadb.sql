a5eb7fd7fc221091207279355122b7451b80ed5d
babelouest <github@babelouest.org>
2019-08-28 15:53:11 -0400

    Add columns gsuc_x509_certificate_name and rename gsuc_x509_certificate to gsuc_x509_certificate_content

--- src/scheme/certificate.mariadb.sql
+++ src/scheme/certificate.mariadb.sql
@@ -6,9 +6,10 @@ CREATE TABLE gs_user_certificate (
   gsuc_issued_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   gsuc_username VARCHAR(128) NOT NULL,
   gsuc_enabled TINYINT(1) DEFAULT 1,
-  gsuc_x509_certificate BLOB DEFAULT NULL,
+  gsuc_x509_certificate_name VARCHAR(128) NOT NULL,
+  gsuc_x509_certificate_content BLOB DEFAULT NULL,
   gsuc_x509_certificate_id VARCHAR(128) NOT NULL,
-  gsuc_expiration TIMESTAMP
+  gsuc_expiration TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX i_gsuc_username ON gs_user_certificate(gsuc_username);
 CREATE INDEX i_gsuc_x509_certificate_id ON gs_user_certificate(gsuc_x509_certificate_id);
--- src/scheme/certificate.postgre.sql
+++ src/scheme/certificate.postgre.sql
@@ -6,9 +6,10 @@ CREATE TABLE gs_user_certificate (
   gsuc_issued_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   gsuc_username VARCHAR(128) NOT NULL,
   gsuc_enabled TINYINT(1) DEFAULT 1,
-  gsuc_x509_certificate TEXT DEFAULT NULL,
+  gsuc_x509_certificate_name VARCHAR(128) NOT NULL,
+  gsuc_x509_certificate_content TEXT DEFAULT NULL,
   gsuc_x509_certificate_id VARCHAR(128) NOT NULL,
-  gsuc_expiration TIMESTAMPTZ
+  gsuc_expiration TIMESTAMPTZ DEFAULT NOW()
 );
 CREATE INDEX i_gsuc_username ON gs_user_certificate(gsuc_username);
 CREATE INDEX i_gsuc_x509_certificate_id ON gs_user_certificate(gsuc_x509_certificate_id);
