4fac48f1997e38766822913cbde8b4311fa32d43
babelouest <github@babelouest.org>
2019-08-28 15:53:11 -0400

    Add columns gsuc_x509_certificate_name and rename gsuc_x509_certificate to gsuc_x509_certificate_content

--- src/scheme/certificate.sqlite.sql
+++ src/scheme/certificate.sqlite.sql
@@ -6,9 +6,10 @@ CREATE TABLE gs_user_certificate (
   gsuc_issued_at TIMESTAMP NOT NULL DEFAULT NOW(),
   gsuc_username TEXT NOT NULL,
   gsuc_enabled INTEGER DEFAULT 1,
-  gsuc_x509_certificate TEXT DEFAULT NULL,
+  gsuc_x509_certificate_name TEXT DEFAULT NULL,
+  gsuc_x509_certificate_content TEXT DEFAULT NULL,
   gsuc_x509_certificate_id TEXT NOT NULL,
-  gsuc_expiration TIMESTAMP
+  gsuc_expiration TIMESTAMP DEFAULT NOW()
 );
 CREATE INDEX i_gsuc_username ON gs_user_certificate(gsuc_username);
 CREATE INDEX i_gsuc_x509_certificate_id ON gs_user_certificate(gsuc_x509_certificate_id);
