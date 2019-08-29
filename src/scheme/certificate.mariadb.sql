7045825d68ec7d788300234993439cca60e52e5a
babelouest <github@babelouest.org>
2019-08-29 16:30:07 -0400

    Update certificate sql table

--- src/scheme/certificate.mariadb.sql
+++ src/scheme/certificate.mariadb.sql
@@ -3,13 +3,14 @@ DROP TABLE IF EXISTS gs_user_certificate;
 CREATE TABLE gs_user_certificate (
   gsuc_id INT(11) PRIMARY KEY AUTO_INCREMENT,
   gsuc_mod_name VARCHAR(128) NOT NULL,
-  gsuc_issued_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   gsuc_username VARCHAR(128) NOT NULL,
   gsuc_enabled TINYINT(1) DEFAULT 1,
-  gsuc_x509_certificate_name VARCHAR(128) NOT NULL,
   gsuc_x509_certificate_content BLOB DEFAULT NULL,
   gsuc_x509_certificate_id VARCHAR(128) NOT NULL,
-  gsuc_expiration TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  gsuc_activation TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gsuc_expiration TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gsuc_last_used TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gsuc_last_user_agent VARCHAR(512) DEFAULT NULL
 );
 CREATE INDEX i_gsuc_username ON gs_user_certificate(gsuc_username);
 CREATE INDEX i_gsuc_x509_certificate_id ON gs_user_certificate(gsuc_x509_certificate_id);
--- src/scheme/certificate.postgre.sql
+++ src/scheme/certificate.postgre.sql
@@ -3,13 +3,14 @@ DROP TABLE IF EXISTS gs_user_certificate;
 CREATE TABLE gs_user_certificate (
   gsuc_id SERIAL PRIMARY KEY,
   gsuc_mod_name VARCHAR(128) NOT NULL,
-  gsuc_issued_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   gsuc_username VARCHAR(128) NOT NULL,
   gsuc_enabled TINYINT(1) DEFAULT 1,
-  gsuc_x509_certificate_name VARCHAR(128) NOT NULL,
   gsuc_x509_certificate_content TEXT DEFAULT NULL,
   gsuc_x509_certificate_id VARCHAR(128) NOT NULL,
-  gsuc_expiration TIMESTAMPTZ DEFAULT NOW()
+  gsuc_activation TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  gsuc_expiration TIMESTAMPTZ DEFAULT NOW(),
+  gsuc_last_used TIMESTAMPTZ DEFAULT NOW(),
+  gsuc_last_user_agent VARCHAR(512) DEFAULT NULL
 );
 CREATE INDEX i_gsuc_username ON gs_user_certificate(gsuc_username);
 CREATE INDEX i_gsuc_x509_certificate_id ON gs_user_certificate(gsuc_x509_certificate_id);
