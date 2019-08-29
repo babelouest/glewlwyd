5ab5b8ca811453643526e2742546bb5ba03e55d6
babelouest <github@babelouest.org>
2019-08-29 16:30:07 -0400

    Update certificate sql table

--- src/scheme/certificate.sqlite.sql
+++ src/scheme/certificate.sqlite.sql
@@ -3,13 +3,14 @@ DROP TABLE IF EXISTS gs_user_certificate;
 CREATE TABLE gs_user_certificate (
   gsuc_id INTEGER PRIMARY KEY AUTOINCREMENT,
   gsuc_mod_name TEXT NOT NULL,
-  gsuc_issued_at TIMESTAMP NOT NULL DEFAULT NOW(),
   gsuc_username TEXT NOT NULL,
   gsuc_enabled INTEGER DEFAULT 1,
-  gsuc_x509_certificate_name TEXT DEFAULT NULL,
   gsuc_x509_certificate_content TEXT DEFAULT NULL,
   gsuc_x509_certificate_id TEXT NOT NULL,
-  gsuc_expiration TIMESTAMP DEFAULT NOW()
+  gsuc_activation TIMESTAMP NOT NULL DEFAULT NOW(),
+  gsuc_expiration TIMESTAMP DEFAULT NOW(),
+  gsuc_last_used TIMESTAMP DEFAULT NOW(),
+  gsuc_last_user_agent TEXT DEFAULT NULL
 );
 CREATE INDEX i_gsuc_username ON gs_user_certificate(gsuc_username);
 CREATE INDEX i_gsuc_x509_certificate_id ON gs_user_certificate(gsuc_x509_certificate_id);
