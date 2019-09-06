1c34073f526fed0fcb0b2ee0549812e775d89058
babelouest <github@babelouest.org>
2019-09-06 10:54:43 -0400

    Add scheme certitfiate table in the main init script

--- docs/database/init.postgre.sql
+++ docs/database/init.postgre.sql
@@ -42,6 +42,7 @@ DROP TABLE IF EXISTS gs_webauthn_assertion;
 DROP TABLE IF EXISTS gs_webauthn_credential;
 DROP TABLE IF EXISTS gs_webauthn_user;
 DROP TABLE IF EXISTS gs_otp;
+DROP TABLE IF EXISTS gs_user_certificate;
 
 CREATE TABLE g_user_module_instance (
   gumi_id SERIAL PRIMARY KEY,
@@ -447,6 +448,23 @@ CREATE TABLE gs_otp (
   gso_totp_time_step_size INTEGER
 );
 CREATE INDEX i_gsso_username ON gs_otp(gso_username);
+
+CREATE TABLE gs_user_certificate (
+  gsuc_id SERIAL PRIMARY KEY,
+  gsuc_mod_name VARCHAR(128) NOT NULL,
+  gsuc_username VARCHAR(128) NOT NULL,
+  gsuc_enabled SMALLINT DEFAULT 1,
+  gsuc_x509_certificate_content TEXT DEFAULT NULL,
+  gsuc_x509_certificate_id VARCHAR(128) NOT NULL,
+  gsuc_x509_certificate_dn VARCHAR(512) NOT NULL,
+  gsuc_x509_certificate_issuer_dn VARCHAR(512) NOT NULL,
+  gsuc_activation TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  gsuc_expiration TIMESTAMPTZ DEFAULT NOW(),
+  gsuc_last_used TIMESTAMPTZ DEFAULT NOW(),
+  gsuc_last_user_agent VARCHAR(512) DEFAULT NULL
+);
+CREATE INDEX i_gsuc_username ON gs_user_certificate(gsuc_username);
+CREATE INDEX i_gsuc_x509_certificate_id ON gs_user_certificate(gsuc_x509_certificate_id);
 
 INSERT INTO g_scope (gs_name, gs_display_name, gs_description, gs_password_required, gs_password_max_age) VALUES ('g_admin', 'Glewlwyd administration', 'Access to Glewlwyd''s administration API', 1, 600);
 INSERT INTO g_scope (gs_name, gs_display_name, gs_description, gs_password_required, gs_password_max_age) VALUES ('g_profile', 'Glewlwyd profile', 'Access to the user''s profile API', 1, 600);
