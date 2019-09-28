db1d5a935df20d0392d9a50f0dc5e30209e06e57
babelouest <github@babelouest.org>
2019-09-28 14:32:05 -0400

    Fix webpack config indent

--- webapp-src/webpack.config.js
+++ webapp-src/webpack.config.js
@@ -3,55 +3,55 @@ var webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-		entry: {
-      admin: './src/admin.js',
-      login: './src/login.js',
-      profile: './src/profile.js'
-    },
-		output: {
-				path: path.resolve(__dirname, 'output'),
-				filename: '[name].js',
-				libraryTarget: 'umd'
-		},
+  mode: 'production',
+  entry: {
+    admin: './src/admin.js',
+    login: './src/login.js',
+    profile: './src/profile.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'output'),
+    filename: '[name].js',
+    libraryTarget: 'umd'
+  },
 
-		module: {
-				rules: [
-						{
-								test: /\.js$/,
-								exclude: /(node_modules|bower_components|build)/,
-								use: {
-										loader: 'babel-loader',
-										options: {
-												presets: ['env']
-										}
-								}
-						},
-						{
-								test: /\.css$/,
-								loader: 'style-loader!css-loader'
-						}
-				]
-		},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
 
-		 plugins: [
-				new webpack.DefinePlugin({
-						"process.env": { 
-								NODE_ENV: JSON.stringify("production") 
-						}
-				}),
-				new UglifyJsPlugin({
-					test: /\.js($|\?)/i,
-					sourceMap: true,
-					uglifyOptions: {
-						mangle: {
-							keep_fnames: true
-						},
-            warnings: false,
-						output: {
-							beautify: false
-						}
-					}
-				})
-		]
+   plugins: [
+    new webpack.DefinePlugin({
+      "process.env": { 
+        NODE_ENV: JSON.stringify("production") 
+      }
+    }),
+    new UglifyJsPlugin({
+      test: /\.js($|\?)/i,
+      sourceMap: true,
+      uglifyOptions: {
+      mangle: {
+        keep_fnames: true
+      },
+      warnings: false,
+      output: {
+        beautify: false
+      }
+      }
+    })
+  ]
 }
