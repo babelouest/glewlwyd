d6bddfe80d16f818c83d282ac67757f7adc4452a
babelouest <github@babelouest.org>
2019-09-30 23:54:51 -0400

    Add 'es2015' in webpack presets

--- webapp-src/webpack.config.js
+++ webapp-src/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['es2015','env']
           }
         }
       },
