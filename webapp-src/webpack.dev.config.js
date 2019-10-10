45e61ef68485b5dd13af5410cea0584339f58f02
babelouest <github@babelouest.org>
2019-10-09 21:18:21 -0400

    Fix indent on webpack config files

--- webapp-src/webpack.dev.config.js
+++ webapp-src/webpack.dev.config.js
@@ -1,43 +1,43 @@
 var path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    admin: './src/admin.js',
-    login: './src/login.js',
-    profile: './src/profile.js'
-  },
-  devtool: 'inline-source-map',
-  output: {
-    path: path.resolve(__dirname),
-    filename: '[name].js',
-    libraryTarget: 'umd'
-  },
+	mode: 'development',
+	entry: {
+		admin: './src/admin.js',
+		login: './src/login.js',
+		profile: './src/profile.js'
+	},
+	devtool: 'inline-source-map',
+	output: {
+		path: path.resolve(__dirname),
+		filename: '[name].js',
+		libraryTarget: 'umd'
+	},
 
-  devServer: {
-    contentBase: path.resolve(__dirname),
-    compress: true,
-    port: 3000,
-    host: 'localhost',
-    open: true
-  },
+	devServer: {
+		contentBase: path.resolve(__dirname),
+		compress: true,
+		port: 3000,
+		host: 'localhost',
+		open: true
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015','env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }
-    ]
-  }
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components|build)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['es2015','env']
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader'
+			}
+		]
+	}
 }
