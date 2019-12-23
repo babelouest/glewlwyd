87179fa1bec2b3d2008053efb3a24fb03ee59c55
babelouest <github@babelouest.org>
2019-12-22 20:04:09 -0500

    Implement e-mail verification for registration in UI

--- webapp-src/src/Profile/Register.js
+++ webapp-src/src/Profile/Register.js
@@ -15,9 +15,15 @@ class Register extends Component {
       registerSchemes: props.registerSchemes,
       username: "",
       usernameValid: false,
+      verificationSent: false,
+      email: "",
+      code: "",
+      invalidCode: false,
+      showCode: false,
       timeout: false,
       checkingUsername: false,
       invalidMessage: false,
+      invalidEmailMessage: true,
       password: "",
       passwordConfirm: "",
       invalidPassword: false,
@@ -69,17 +75,22 @@ class Register extends Component {
     });
   }
   
+  changeEmailVerification(e) {
+    var usernameValid = this.state.usernameValid;
+    if (this.state.registerConfig["email-is-username"]) {
+      usernameValid = true;
+    }
+    this.setState({email: e.target.value, invalidEmailMessage: !e.target.value, usernameValid: usernameValid});
+  }
+  
+  changeCode(e) {
+    this.setState({code: e.target.value, invalidCode: !e.target.value});
+  }
+  
   changeName(e) {
     e.preventDefault();
     var profile = this.state.registerProfile;
     profile.name = e.target.value;
-    this.setState({registerProfile: profile});
-  }
-  
-  changeEmail(e) {
-    e.preventDefault();
-    var profile = this.state.registerProfile;
-    profile.email = e.target.value;
     this.setState({registerProfile: profile});
   }
   
@@ -119,7 +130,7 @@ class Register extends Component {
     });
   }
   
-  SaveProfile(e) {
+  saveProfile(e) {
     e.preventDefault();
     
     apiManager.glewlwydRequest("/" + this.state.config.params.register + "/profile", "PUT", this.state.registerProfile)
@@ -139,10 +150,37 @@ class Register extends Component {
         messageDispatcher.sendMessage('App', {type: "registration"});
       }
     })
-    .fail(() => {
+    .fail((err) => {
       if (err.status !== 400) {
         messageDispatcher.sendMessage('Notification', {type: "danger", message: i18next.t("error-api-connect")});
       }
+    });
+  }
+  
+  sendVerificationEmail() {
+    apiManager.glewlwydRequest("/" + this.state.config.params.register + "/verify", "PUT", {username: this.state.username, email: this.state.email})
+    .then(() => {
+      this.setState({verificationSent: true});
+    })
+    .fail((err) => {
+      if (err.status !== 400) {
+        messageDispatcher.sendMessage('Notification', {type: "danger", message: i18next.t("error-api-connect")});
+      }
+    });
+  }
+  
+  verifyCode() {
+    apiManager.glewlwydRequest("/" + this.state.config.params.register + "/verify", "POST", {username: this.state.username, email: this.state.email, code: this.state.code})
+    .then(() => {
+      messageDispatcher.sendMessage('App', {type: "registration"});
+      messageDispatcher.sendMessage('Notification', {type: "info", message: i18next.t("profile.register-profile-created")});
+    })
+    .fail((err) => {
+      this.setState({invalidCode: true}, () => {
+        if (err.status !== 400) {
+          messageDispatcher.sendMessage('Notification', {type: "danger", message: i18next.t("error-api-connect")});
+        }
+      });
     });
   }
   
@@ -151,6 +189,7 @@ class Register extends Component {
     .then(() => {
       this.setState({registerComplete: true, username: ""}, () => {
         messageDispatcher.sendMessage('Notification', {type: "info", message: i18next.t("profile.register-profile-completed")});
+        messageDispatcher.sendMessage('App', {type: "registrationComplete"});
       });
     })
     .fail((err) => {
@@ -165,7 +204,7 @@ class Register extends Component {
   cancelRegistration() {
     apiManager.glewlwydRequest("/" + this.state.config.params.register + "/profile", "DELETE")
     .then(() => {
-      this.setState({username: "", usernameValid: false}, () => {
+      this.setState({username: "", usernameValid: false, email: "", verificationSent: false, code: ""}, () => {
         messageDispatcher.sendMessage('Notification', {type: "info", message: i18next.t("profile.register-profile-cancelled")});
         messageDispatcher.sendMessage('App', {type: "registration"});
       });
@@ -176,7 +215,7 @@ class Register extends Component {
   }
   
   render() {
-    var formJsx, completeMessageJsx, buttonJsx, passwordJsx;
+    var formJsx, completeMessageJsx, buttonJsx, passwordJsx, emailJsx;
     if (this.state.registerComplete) {
       completeMessageJsx = 
       <div>
@@ -237,8 +276,22 @@ class Register extends Component {
           {this.state.invalidPassword?<span className="error-input">{this.state.invalidPassword}</span>:""}
         </div>
       }
+      if (this.state.registerConfig["verify-email"]) {
+        emailJsx =
+        <div>
+          <hr/>
+          <label htmlFor="email-input">{i18next.t("profile.register-email-label")}</label>
+          <div className="input-group">
+            <input type="text" 
+                   className={"form-control"} 
+                   id="email-input"
+                   disabled={true}
+                   value={this.state.registerProfile.email||""}/>
+          </div>
+        </div>
+      }
       formJsx =
-        <form className="needs-validation" noValidate onSubmit={(e) => this.SaveProfile(e)}>
+        <form className="needs-validation" noValidate onSubmit={(e) => this.saveProfile(e)}>
           <label htmlFor="username-input">{i18next.t("profile.register-profile-username-label")}</label>
           <div className="input-group">
             <input type="text" 
@@ -246,6 +299,7 @@ class Register extends Component {
                    value={this.state.registerProfile.username}
                    disabled={true}/>
           </div>
+          {emailJsx}
           <hr/>
           <label htmlFor="name-input">{i18next.t("profile.register-name-label")}</label>
           <div className="input-group">
@@ -255,16 +309,6 @@ class Register extends Component {
                    placeholder={i18next.t("profile.register-name-ph")} 
                    onChange={(e) => this.changeName(e)} 
                    value={this.state.registerProfile.name||""}/>
-          </div>
-          <hr/>
-          <label htmlFor="email-input">{i18next.t("profile.register-email-label")}</label>
-          <div className="input-group">
-            <input type="text" 
-                   className={"form-control"} 
-                   id="email-input"
-                   placeholder={i18next.t("profile.register-email-ph")} 
-                   onChange={(e) => this.changeEmail(e)} 
-                   value={this.state.registerProfile.email||""}/>
           </div>
           {passwordJsx}
           <hr/>
@@ -279,7 +323,7 @@ class Register extends Component {
           <div>
             <button className="btn btn-secondary btn-icon" 
                     type="submit" 
-                    onClick={(e) => this.SaveProfile(e)}
+                    onClick={(e) => this.saveProfile(e)}
                     disabled={this.state.invalidPassword}
                     title={i18next.t("save")}>
               {i18next.t("save")}
@@ -298,28 +342,117 @@ class Register extends Component {
             </button>
           </div>
     } else if (!this.state.registerProfile) {
-      formJsx = 
-        <form className="needs-validation" noValidate>
-          <label htmlFor="username-input">{i18next.t("profile.register-username-label")}</label>
-          <div className="input-group">
-            <input type="text" 
-                   className={"form-control"} 
-                   id="username-input"
-                   placeholder={i18next.t("profile.register-username-ph")} 
-                   onChange={(e) => this.changeUsername(e)} 
-                   value={this.state.username}/>
-            <div className="input-group-append">
-              <button className={"btn" + ((this.state.usernameValid&&!this.state.checkingUsername)?" btn-outline-success":" btn-outline-danger")} 
-                      type="button" 
-                      onClick={() => this.registerUsername()} 
-                      disabled={!this.state.usernameValid}
-                      title={i18next.t("profile.register-username-create")}>
-                <i className={(this.state.checkingUsername?"fas fa-compass fa-spin":"fas fa-plus")}></i>
-              </button>
+      if (this.state.registerConfig["verify-email"]) {
+        var buttonVerifyJsx, codeInputJsx;
+        if (this.state.verificationSent) {
+          codeInputJsx =
+            <div>
+              <hr/>
+              <label htmlFor="code-input">{i18next.t("profile.register-code-label")}</label>
+              <div className="input-group">
+                <input type="text" 
+                       className={"form-control"} 
+                       id="code-input"
+                       placeholder={i18next.t("profile.register-code-ph")} 
+                       onChange={(e) => this.changeCode(e)} 
+                       value={this.state.code}/>
+                {this.state.invalidCode?<span className="error-input">{i18next.t("profile.register-code-error")}</span>:""}
+              </div>
             </div>
-            {this.state.invalidMessage?<span className="error-input">{i18next.t("profile.register-username-error")}</span>:""}
-          </div>
-        </form>
+          buttonVerifyJsx = 
+            <button className="btn btn-success" 
+                    type="button" 
+                    onClick={() => this.verifyCode()} 
+                    title={i18next.t("profile.register-profile-verify-code")}>
+              {i18next.t("profile.register-profile-verify-code")}
+            </button>
+        } else {
+          buttonVerifyJsx = 
+            <button className="btn btn-success" 
+                    type="button" 
+                    onClick={() => this.sendVerificationEmail()} 
+                    disabled={!this.state.usernameValid || !this.state.email}
+                    title={i18next.t("profile.register-profile-verify-email")}>
+              {i18next.t("profile.register-profile-verify-email")}
+            </button>
+        }
+        if (this.state.registerConfig["email-is-username"]) {
+          formJsx = 
+            <form className="needs-validation" noValidate>
+              <label htmlFor="email-input">{i18next.t("profile.register-email-label")}</label>
+              <div className="input-group">
+                <input type="text" 
+                       className={"form-control"} 
+                       id="email-input"
+                       placeholder={i18next.t("profile.register-email-ph")} 
+                       onChange={(e) => this.changeEmailVerification(e)} 
+                       disabled={this.state.verificationSent}
+                       value={this.state.email}/>
+                {this.state.invalidEmailMessage?<span className="error-input">{i18next.t("profile.register-email-error")}</span>:""}
+              </div>
+              {codeInputJsx}
+              <hr/>
+              <div className="input-group-append">
+                {buttonVerifyJsx}
+              </div>
+            </form>
+        } else {
+          formJsx = 
+            <form className="needs-validation" noValidate>
+              <label htmlFor="username-input">{i18next.t("profile.register-username-label")}</label>
+              <div className="input-group">
+                <input type="text" 
+                       className={"form-control"} 
+                       id="username-input"
+                       placeholder={i18next.t("profile.register-username-ph")} 
+                       onChange={(e) => this.changeUsername(e)} 
+                       disabled={this.state.verificationSent}
+                       value={this.state.username}/>
+                {this.state.invalidMessage?<span className="error-input">{i18next.t("profile.register-username-error")}</span>:""}
+              </div>
+              <hr/>
+              <label htmlFor="email-input">{i18next.t("profile.register-email-label")}</label>
+              <div className="input-group">
+                <input type="text" 
+                       className={"form-control"} 
+                       id="email-input"
+                       placeholder={i18next.t("profile.register-email-ph")} 
+                       onChange={(e) => this.changeEmailVerification(e)} 
+                       disabled={this.state.verificationSent}
+                       value={this.state.email}/>
+                {this.state.invalidEmailMessage?<span className="error-input">{i18next.t("profile.register-email-error")}</span>:""}
+              </div>
+              {codeInputJsx}
+              <hr/>
+              <div className="input-group-append">
+                {buttonVerifyJsx}
+              </div>
+            </form>
+        }
+      } else {
+        formJsx = 
+          <form className="needs-validation" noValidate>
+            <label htmlFor="username-input">{i18next.t("profile.register-username-label")}</label>
+            <div className="input-group">
+              <input type="text" 
+                     className={"form-control"} 
+                     id="username-input"
+                     placeholder={i18next.t("profile.register-username-ph")} 
+                     onChange={(e) => this.changeUsername(e)} 
+                     value={this.state.username}/>
+              <div className="input-group-append">
+                <button className={"btn" + ((this.state.usernameValid&&!this.state.checkingUsername)?" btn-outline-success":" btn-outline-danger")} 
+                        type="button" 
+                        onClick={() => this.registerUsername()} 
+                        disabled={!this.state.usernameValid}
+                        title={i18next.t("profile.register-username-create")}>
+                  <i className={(this.state.checkingUsername?"fas fa-compass fa-spin":"fas fa-plus")}></i>
+                </button>
+              </div>
+              {this.state.invalidMessage?<span className="error-input">{i18next.t("profile.register-username-error")}</span>:""}
+            </div>
+          </form>
+      }
     }
     return (
       <div>
