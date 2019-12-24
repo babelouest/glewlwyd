292d914945e9359ae27b71234853b38205503a3a
babelouest <github@babelouest.org>
2019-12-24 11:04:02 -0500

    FOrgot to create table gpr_session in init script

--- docs/database/init.sqlite3.sql
+++ docs/database/init.sqlite3.sql
@@ -44,6 +44,7 @@ DROP TABLE IF EXISTS gs_webauthn_user;
 DROP TABLE IF EXISTS gs_otp;
 DROP TABLE IF EXISTS gs_user_certificate;
 DROP TABLE IF EXISTS gs_user_pkcs12;
+DROP TABLE IF EXISTS gpr_session;
 
 CREATE TABLE g_user_module_instance (
   gumi_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -480,6 +481,23 @@ CREATE TABLE gs_user_pkcs12 (
   gsup_user_agent TEXT DEFAULT NULL
 );
 CREATE INDEX i_gsup_username ON gs_user_pkcs12(gsup_username);
+
+CREATE TABLE gpr_session (
+  gprs_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gprs_plugin_name TEXT NOT NULL,
+  gprs_username TEXT NOT NULL,
+  gprs_name TEXT,
+  gprs_email TEXT,
+  gprs_code TEXT,
+  gprs_password_set INTEGER DEFAULT 0,
+  gprs_session_hash TEXT,
+  gprs_token_hash TEXT,
+  gprs_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  gprs_issued_for TEXT, -- IP address or hostname
+  gprs_user_agent TEXT,
+  gprs_enabled INTEGER DEFAULT 1
+);
+CREATE INDEX i_gprs_session_hash ON gpr_session(gprs_session_hash);
 
 INSERT INTO g_scope (gs_name, gs_display_name, gs_description, gs_password_required, gs_password_max_age) VALUES ('g_admin', 'Glewlwyd administration', 'Access to Glewlwyd''s administration API', 1, 600);
 INSERT INTO g_scope (gs_name, gs_display_name, gs_description, gs_password_required, gs_password_max_age) VALUES ('g_profile', 'Glewlwyd profile', 'Access to the user''s profile API', 1, 600);
