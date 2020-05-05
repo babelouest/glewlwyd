48dd040073222646e02015c15426ead4dd185f46
babelouest <github@babelouest.org>
2020-05-04 21:05:09 -0400

    Add device auth for oauth2 plugin

--- src/plugin/protocol_oauth2.sqlite3.sql
+++ src/plugin/protocol_oauth2.sqlite3.sql
@@ -4,6 +4,8 @@ DROP TABLE IF EXISTS gpg_refresh_token_scope;
 DROP TABLE IF EXISTS gpg_refresh_token;
 DROP TABLE IF EXISTS gpg_code_scope;
 DROP TABLE IF EXISTS gpg_code;
+DROP TABLE IF EXISTS gpg_device_authorization_scope;
+DROP TABLE IF EXISTS gpg_device_authorization;
 
 CREATE TABLE gpg_code (
   gpgc_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -77,4 +79,29 @@ CREATE TABLE gpg_access_token_scope (
   gpga_id INT(11),
   gpgas_scope TEXT NOT NULL,
   FOREIGN KEY(gpga_id) REFERENCES gpg_access_token(gpga_id) ON DELETE CASCADE
+);
+
+-- store device authorization requests
+CREATE TABLE gpg_device_authorization (
+  gpgda_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpgda_plugin_name TEXT NOT NULL,
+  gpgda_client_id TEXT NOT NULL,
+  gpgda_username TEXT,
+  gpgda_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gpgda_expires_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gpgda_issued_for TEXT, -- IP address or hostname of the deice client
+  gpgda_device_code_hash TEXT NOT NULL,
+  gpgda_user_code_hash TEXT NOT NULL,
+  gpgda_status INTEGER DEFAULT 0, -- 0: created, 1: user verified, 2 device completed, 3 disabled
+  gpgda_last_check TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX i_gpgda_device_code_hash ON gpg_device_authorization(gpgda_device_code_hash);
+CREATE INDEX i_gpgda_user_code_hash ON gpg_device_authorization(gpgda_user_code_hash);
+
+CREATE TABLE gpg_device_authorization_scope (
+  gpgdas_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpgda_id INTEGER,
+  gpgdas_scope TEXT NOT NULL,
+  gpgdas_allowed INTEGER DEFAULT 0,
+  FOREIGN KEY(gpgda_id) REFERENCES gpg_device_authorization(gpgda_id) ON DELETE CASCADE
 );
