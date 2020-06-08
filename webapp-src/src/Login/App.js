e0aa3cec2506fcab7aa29650eb777b4a35722c76
yrammos <y@rammos.co>
2020-06-08 16:39:29 +0300

    Restore inadvertently omitted hamburger button.

--- webapp-src/src/Login/App.js
+++ webapp-src/src/Login/App.js
@@ -302,6 +302,9 @@ class App extends Component {
                   <img className="mr-3" src="img/logo-login.png" alt="logo"/>
                   {i18next.t("login.menu-title")}
                 </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul className="navbar-nav mr-auto">
                     {/* Placeholder */}
