60ff74e143d87c69aeb12aeb66760685404afde5
yrammos <y@rammos.co>
2020-06-16 12:03:43 +0000

    Delete console.log's and merge consecutive setState's.

--- webapp-src/src/Admin/App.js
+++ webapp-src/src/Admin/App.js
@@ -104,8 +104,7 @@ class App extends Component {
         this.setState({loggedIn: message.message}, () => {
           console.log("loggedIn", this.state.loggedIn);
           if (!this.state.loggedIn) {
-            this.setState({curNav: "users"});
-            this.setState({profileList: this.state.profileList.filter((e,i) => i !== 0)}) 
+            this.setState({curNav: "users", profileList: this.state.profileList.filter((e,i) => i !== 0)});
           } else {
             this.fetchApi();
           }
@@ -1336,9 +1335,6 @@ class App extends Component {
   }
 
 	render() {
-        console.log('Admin APP state');
-    console.log(this.state);
-    console.log(this.state.profileList);
     var invalidCredentialMessage;
     if (this.state.invalidCredentialMessage) {
       invalidCredentialMessage = <div className="alert alert-danger" role="alert">{i18next.t("admin.error-credential-message")}</div>
