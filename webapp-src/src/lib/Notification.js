05e693b05c61eac4d4099a2f20dfeb25332e19a8
babelouest <github@babelouest.org>
2020-06-24 17:29:16 -0400

    Upgrade to bootstrap 4.5, notifications are broken

--- webapp-src/src/lib/Notification.js
+++ webapp-src/src/lib/Notification.js
@@ -34,7 +34,8 @@ class Notification extends Component {
   timeoutClose(id) {
     setTimeout(() => {
       this.close(id);
-    }, 5000);
+//    }, 5000);
+    }, 150000);
   }
   
   close(id) {
@@ -102,9 +103,42 @@ class Notification extends Component {
         );
       });
     }
-    return (
+    /*return (
       <div>
         {toast}
+      </div>
+    );*/
+    return (
+      <div aria-live="polite" aria-atomic="true">
+        <div style={{position: "absolute", top: 0, right: 0}}>
+          <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div className="toast-header">
+              <img src="..." className="rounded mr-2" alt="..."/>
+              <strong className="mr-auto">Bootstrap</strong>
+              <small className="text-muted">just now</small>
+              <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="toast-body">
+              See? Just like this.
+            </div>
+          </div>
+
+          <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div className="toast-header">
+              <img src="..." className="rounded mr-2" alt="..."/>
+              <strong className="mr-auto">Bootstrap</strong>
+              <small className="text-muted">2 seconds ago</small>
+              <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="toast-body">
+              Heads up, toasts will stack automatically
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
