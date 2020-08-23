2098c21e45f7e3f8204260dfd373b48c9865a656
babelouest <github@babelouest.org>
2020-08-23 13:58:01 -0400

    Fix bug in init.sqlite3.sql, update g_user_module_instance initialization with new property reset-credentials-code

--- docs/database/init.sqlite3.sql
+++ docs/database/init.sqlite3.sql
@@ -635,64 +635,6 @@ CREATE TABLE gpr_reset_credentials_session (
   gprrcs_plugin_name TEXT NOT NULL,
   gprrcs_username TEXT NOT NULL,
   gprrcs_session_hash TEXT,
-  gprrcs_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  gprrcs_issued_for TEXT, -- IP address or hostname
-  gprrcs_user_agent TEXT,
-  gprrcs_enabled INTEGER DEFAULT 1
-);
-CREATE INDEX i_gprrcs_session_hash ON gpr_reset_credentials_session(gprrcs_session_hash);
-
-CREATE TABLE gpr_reset_credentials_email (
-  gprrct_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  gprrct_plugin_name TEXT NOT NULL,
-  gprrct_username TEXT NOT NULL,
-  gprrct_token_hash TEXT,
-  gprrct_callback_url TEXT DEFAULT NULL,
-  gprrct_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  gprrct_issued_for TEXT, -- IP address or hostname
-  gprrct_user_agent TEXT,
-  gprrct_enabled INTEGER DEFAULT 1
-);
-CREATE INDEX i_gprrct_token_hash ON gpr_reset_credentials_email(gprrct_token_hash);
-
-CREATE TABLE gpr_session (
-  gprs_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  gprs_plugin_name TEXT NOT NULL,
-  gprs_username TEXT NOT NULL,
-  gprs_name TEXT,
-  gprs_email TEXT,
-  gprs_code_hash TEXT,
-  gprs_callback_url TEXT DEFAULT NULL,
-  gprs_password_set INTEGER DEFAULT 0,
-  gprs_session_hash TEXT,
-  gprs_token_hash TEXT,
-  gprs_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  gprs_issued_for TEXT, -- IP address or hostname
-  gprs_user_agent TEXT,
-  gprs_enabled INTEGER DEFAULT 1
-);
-CREATE INDEX i_gprs_session_hash ON gpr_session(gprs_session_hash);
-CREATE INDEX i_gprs_gprs_token_hash ON gpr_session(gprs_token_hash);
-CREATE INDEX i_gprs_gprs_gprs_code_hash ON gpr_session(gprs_code_hash);
-
-CREATE TABLE gpr_update_email (
-  gprue_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  gprue_plugin_name TEXT NOT NULL,
-  gprue_username TEXT NOT NULL,
-  gprue_email TEXT,
-  gprue_token_hash TEXT,
-  gprue_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  gprue_issued_for TEXT, -- IP address or hostname
-  gprue_user_agent TEXT,
-  gprue_enabled INTEGER DEFAULT 1
-);
-CREATE INDEX i_gprue_token_hash ON gpr_update_email(gprue_token_hash);
-
-CREATE TABLE gpr_reset_credentials_session (
-  gprrcs_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  gprrcs_plugin_name TEXT NOT NULL,
-  gprrcs_username TEXT NOT NULL,
-  gprrcs_session_hash TEXT,
   gprrcs_callback_url TEXT DEFAULT NULL,
   gprrcs_expires_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   gprrcs_issued_for TEXT, -- IP address or hostname
@@ -738,7 +680,7 @@ CREATE TABLE gs_oauth2_session (
 INSERT INTO g_scope (gs_name, gs_display_name, gs_description, gs_password_required, gs_password_max_age) VALUES ('g_admin', 'Glewlwyd administration', 'Access to Glewlwyd''s administration API', 1, 600);
 INSERT INTO g_scope (gs_name, gs_display_name, gs_description, gs_password_required, gs_password_max_age) VALUES ('g_profile', 'Glewlwyd profile', 'Access to the user''s profile API', 1, 600);
 INSERT INTO g_scope (gs_name, gs_display_name, gs_description, gs_password_required, gs_password_max_age) VALUES ('openid', 'Open ID', 'Open ID Connect scope', 0, 0);
-INSERT INTO g_user_module_instance (gumi_module, gumi_order, gumi_name, gumi_display_name, gumi_parameters, gumi_readonly) VALUES ('database', 0, 'database', 'Database backend', '{"use-glewlwyd-connection":true,"data-format":{"picture":{"multiple":false,"read":true,"write":true,"profile-read":true,"profile-write":true}}}', 0);
+INSERT INTO g_user_module_instance (gumi_module, gumi_order, gumi_name, gumi_display_name, gumi_parameters, gumi_readonly) VALUES ('database', 0, 'database', 'Database backend', '{"use-glewlwyd-connection":true,"data-format":{"picture":{"multiple":false,"read":true,"write":true,"profile-read":true,"profile-write":true},"reset-credentials-code":{"multiple":false,"read":true,"write":true,"profile-read":false,"profile-write":false}}}', 0);
 INSERT INTO g_client_module_instance (gcmi_module, gcmi_order, gcmi_name, gcmi_display_name, gcmi_parameters, gcmi_readonly) VALUES ('database', 0, 'database', 'Database backend', '{"use-glewlwyd-connection":true,"data-format":{"redirect_uri":{"multiple":true,"read":true,"write":true},"authorization_type":{"multiple":true,"read":true,"write":true},"client_secret":{"read":true,"write":true},"pubkey":{"read":true,"write":true},"jwks":{"convert":"jwks","read":true,"write":true},"jwks_uri":{"read":true,"write":true},"post_logout_redirect_uris":{"multiple":true,"read":true,"write":true},"token_endpoint_auth_method":{"multiple":false,"read":true,"write":true}}}', 0);
 INSERT INTO g_user (gu_username, gu_name, gu_password, gu_email, gu_enabled) VALUES ('admin', 'The Administrator', 'fOfvZC/wR2cUSTWbW6YZueGyyDuFqwkoFlcNlRYWJscxYTVOVFJ3VWFHdVJQT0pU', '', 1);
 INSERT INTO g_user_scope (gus_name) VALUES ('g_admin');
