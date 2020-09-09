b16425f59147d85564de2a895a848ab94fdb62e1
babelouest <github@babelouest.org>
2020-09-09 15:13:32 -0400

    Run glewlwyd in docker image with env variables allowed

--- docs/docker/entrypoint.sh
+++ docs/docker/entrypoint.sh
@@ -5,4 +5,4 @@ if [ ! -f "/var/cache/glewlwyd/glewlwyd.db" ]; then
   sqlite3 /var/cache/glewlwyd/glewlwyd.db < /usr/share/glewlwyd/docs/database/init.sqlite3.sql
 fi
 
-glewlwyd --config-file=/etc/glewlwyd/glewlwyd.conf -mconsole
+glewlwyd --config-file=/etc/glewlwyd/glewlwyd.conf -mconsole -e
