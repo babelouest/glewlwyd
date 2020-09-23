5e8835d5f82148eee0a666b708f5a818dd80b467
babelouest <github@babelouest.org>
2020-09-23 13:53:20 -0400

    Fix user_agent declaration in callback_glewlwyd_add_api_key
    One point for my clumsiness

--- src/webservice.c
+++ src/webservice.c
@@ -2296,7 +2296,7 @@ int callback_glewlwyd_get_api_key_list (const struct _u_request * request, struc
 
 int callback_glewlwyd_add_api_key (const struct _u_request * request, struct _u_response * response, void * user_data) {
   struct config_elements * config = (struct config_elements *)user_data;
-  const char * issued_for = get_ip_source(request), * username = json_string_value(json_object_get((json_t *)response->shared_data, "username")), user_agent = u_map_get_case(request->map_header, "user-agent");
+  const char * issued_for = get_ip_source(request), * username = json_string_value(json_object_get((json_t *)response->shared_data, "username")), * user_agent = u_map_get_case(request->map_header, "user-agent");
   json_t * j_api_key = generate_api_key(config, username, issued_for, user_agent);
   
   if (check_result_value(j_api_key, G_OK)) {
