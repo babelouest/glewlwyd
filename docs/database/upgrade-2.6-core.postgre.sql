344780586e770234bde11980ad4dbd51fb4e4977
babelouest <github@babelouest.org>
2021-06-04 17:14:17 -0400

    Add sql upgrade scripts for next version 2.6

---
+++ docs/database/upgrade-2.6-core.postgre.sql
@@ -0,0 +1,9 @@
+-- ----------------------------------------------------- --
+-- Upgrade Glewlwyd 2.5.0 2.6.0
+-- Copyright 2021 Nicolas Mora <mail@babelouest.org>     --
+-- License: MIT                                          --
+-- ----------------------------------------------------- --
+
+ALTER TABLE g_user_auth_scheme_module_instance
+ADD guasmi_forbid_user_profile SMALLINT DEFAULT 0,
+ADD guasmi_forbid_user_reset_credential SMALLINT DEFAULT 0;
