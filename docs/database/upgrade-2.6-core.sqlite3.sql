82d91dc9f5958d70ae650f1b50fbfdc5e3a69a2e
babelouest <github@babelouest.org>
2021-10-29 17:08:28 -0400

    Update sqlite3 script to upgrade to 2.6

--- docs/database/upgrade-2.6-core.sqlite3.sql
+++ docs/database/upgrade-2.6-core.sqlite3.sql
@@ -5,7 +5,9 @@
 -- ----------------------------------------------------- --
 
 ALTER TABLE g_user_auth_scheme_module_instance
-ADD guasmi_forbid_user_profile INTEGER DEFAULT 0,
+ADD guasmi_forbid_user_profile INTEGER DEFAULT 0;
+
+ALTER TABLE g_user_auth_scheme_module_instance
 ADD guasmi_forbid_user_reset_credential INTEGER DEFAULT 0;
 
 CREATE TABLE g_user_middleware_module_instance (
@@ -60,15 +62,49 @@ CREATE TABLE gpo_ciba_scheme (
 );
 
 ALTER TABLE gpo_code
-ADD gpoc_s_hash TEXT,
+ADD gpoc_s_hash TEXT;
+
+ALTER TABLE gpo_code
 ADD gpoc_sid TEXT;
 
 ALTER TABLE gpo_id_token
-ADD gpoc_id INTEGER,
-ADD gpor_id INTEGER,
-ADD gpoi_sid TEXT,
-ADD FOREIGN KEY(gpoc_id) REFERENCES gpo_code(gpoc_id) ON DELETE CASCADE,
-ADD FOREIGN KEY(gpor_id) REFERENCES gpo_refresh_token(gpor_id) ON DELETE CASCADE;
+ADD gpoc_id INTEGER;
+
+ALTER TABLE gpo_id_token
+ADD gpor_id INTEGER;
+
+ALTER TABLE gpo_id_token
+ADD gpoi_sid TEXT;
 
 ALTER TABLE gpo_device_authorization
 Add gpoda_sid TEXT;
+
+PRAGMA foreign_keys=off;
+BEGIN TRANSACTION;
+
+ALTER TABLE gpo_id_token RENAME TO _gpo_id_token_old;
+
+CREATE TABLE gpo_id_token (
+  gpoi_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  gpoc_id INTEGER,
+  gpor_id INTEGER,
+  gpoi_plugin_name TEXT NOT NULL,
+  gpoi_authorization_type INTEGER NOT NULL,
+  gpoi_username TEXT,
+  gpoi_client_id TEXT,
+  gpoi_issued_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  gpoi_issued_for TEXT, -- IP address or hostname
+  gpoi_user_agent TEXT,
+  gpoi_hash TEXT,
+  gpoi_sid TEXT,
+  gpoi_enabled INTEGER DEFAULT 1,
+  FOREIGN KEY(gpoc_id) REFERENCES gpo_code(gpoc_id) ON DELETE CASCADE,
+  FOREIGN KEY(gpor_id) REFERENCES gpo_refresh_token(gpor_id) ON DELETE CASCADE
+);
+
+INSERT INTO gpo_id_token SELECT * FROM _gpo_id_token_old;
+
+DROP TABLE _gpo_id_token_old;
+
+COMMIT;
+PRAGMA foreign_keys=on;
