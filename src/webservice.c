742a795626b970c1caa1c0eba573c09f8618ceb9
babelouest <github@babelouest.org>
2022-02-01 19:11:06 -0500

    Add precision on who removed the user

--- src/webservice.c
+++ src/webservice.c
@@ -2030,7 +2030,7 @@ int callback_glewlwyd_delete_user (const struct _u_request * request, struct _u_
       y_log_message(Y_LOG_LEVEL_ERROR, "callback_glewlwyd_delete_user - Error delete_user");
       response->status = 500;
     } else {
-      y_log_message(Y_LOG_LEVEL_INFO, "Event - User '%s' removed", u_map_get(request->map_url, "username"));
+      y_log_message(Y_LOG_LEVEL_INFO, "Event - User '%s' removed - admin action", u_map_get(request->map_url, "username"));
     }
   } else if (check_result_value(j_search_user, G_ERROR_NOT_FOUND)) {
     response->status = 404;
