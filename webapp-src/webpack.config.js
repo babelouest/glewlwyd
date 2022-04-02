312a03ca486371ff7e7f25e36969d34a25ac018d
babelouest <github@babelouest.org>
2022-04-02 16:06:01 -0400

    Upgrade to webpack 5, remove polyfill builder

--- webapp-src/webpack.config.js
+++ webapp-src/webpack.config.js
@@ -7,7 +7,7 @@
  * 
  */
 
-var path = require('path');
+/*var path = require('path');
 var webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
@@ -69,4 +69,57 @@ module.exports = {
 			chunks: 'all'
 		}
 	}
-}
+}*/
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+		admin: path.resolve(__dirname, 'src/admin.js'),
+		login: path.resolve(__dirname, 'src/login.js'),
+		profile: path.resolve(__dirname, 'src/profile.js'),
+		callback: path.resolve(__dirname, 'src/callback.js')
+	},
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+		path: path.resolve(__dirname, 'output'),
+		filename: '[name].js',
+		libraryTarget: 'umd'
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    static: path.resolve(__dirname ),
+    hot: true,
+    compress: true,
+    port: 3000,
+    host: 'localhost',
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4593/',
+        secure: false,
+        changeOrigin: true
+      },
+      '/config': {
+        target: 'http://localhost:4593/',
+        secure: false,
+        changeOrigin: true
+      }
+    }
+  }
+};
