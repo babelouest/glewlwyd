5aea3dab5b734c2af0a61896561906f81db9ea86
babelouest <github@babelouest.org>
2022-04-08 16:44:35 -0400

    Add DPoP as allowed header

--- src/webservice.c
+++ src/webservice.c
@@ -32,7 +32,7 @@ int callback_glewlwyd_options (const struct _u_request * request, struct _u_resp
   UNUSED(request);
   UNUSED(user_data);
   ulfius_add_header_to_response(response, "Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  ulfius_add_header_to_response(response, "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Bearer, Authorization");
+  ulfius_add_header_to_response(response, "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Bearer, Authorization, DPoP");
   ulfius_add_header_to_response(response, "Access-Control-Max-Age", "1800");
   return U_CALLBACK_COMPLETE;
 }
