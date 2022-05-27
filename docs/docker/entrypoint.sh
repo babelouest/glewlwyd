1354677c743147843b6462d952ba0615c819da35
babelouest <github@babelouest.org>
2022-05-27 13:51:22 -0400

    Fix docker entrypoint.sh

--- docs/docker/entrypoint.sh
+++ docs/docker/entrypoint.sh
@@ -5,4 +5,7 @@ if [ ! -f "/var/cache/glewlwyd/glewlwyd.db" ]; then
   sqlite3 /var/cache/glewlwyd/glewlwyd.db < /usr/share/glewlwyd/docs/database/init.sqlite3.sql
 fi
 
+rm /usr/share/glewlwyd/webapp/config.json
+cp /etc/glewlwyd/config.json /usr/share/glewlwyd/webapp/config.json
+
 glewlwyd --config-file=/etc/glewlwyd/glewlwyd.conf -mconsole -e
